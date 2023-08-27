d7292ede05f55e5fdecfb26abb924251bb463cc9
Liang-Xiao-pku <xiaoliangmit@gmail.com>
2023-08-27 22:30:20 +0800

    small changes

--- EuclideanGeometry/Axioms/Lines.lean
+++ EuclideanGeometry/Axioms/Lines.lean
@@ -1,15 +1,18 @@
-/- Define Euclidean plane -/
+/- Define Euclidean plane as normed vector space over ℝ of dimension 2 -/
 
 /- 1.1 Define points, and functions to get the x,y-coordinates -/
 
 /- 1.2 Segments, rays, and lines -/
 
+/- Define vectors in dimension 2 -/
 
 /- Define unit vectors (for directions) -/
 
-/- Define oriented segments, including "carrier" and endpoints, and directions (a unit vector). WE DO NOT ALLOW DEGENERATE SEGMENTS!!!!  to be discussed???? -/
+/- Define oriented segments, including "carrier" and endpoints, and directions (a unit vector).  Here we only allow nondegenerate oriented segements. Next, we define oriented segments_generalized (which only remebers the starting and end points), which allows the two points to be identified; and view oriented segments as an instance.  -/
 
-/- Define whether an oriented segment is degenerate -/
+/- Define the interior of an oriented segment -/
+
+
 
 /- Define rays, including "carrier", endpoints, and directions. -/
 
@@ -18,3 +21,6 @@
 /- Relations between these concepts -/
 
 
+
+
+/- Archimedean properties: on a ray/line, one can always find a far away point, and on an oriented segment, one can always find a point in the interior. -/
