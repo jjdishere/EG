a0435c1a9984560037caa287c623664d471a0ac9
jjdishere <emailboxofjjd@163.com>
2023-08-29 03:52:50 +0800

    changed def of EuclideanPlane
    add the concept of vector space as the difference of pts of Euclidean Plane

--- EuclideanGeometry/Axioms/Basics.lean
+++ EuclideanGeometry/Axioms/Basics.lean
@@ -3,7 +3,7 @@ import Mathlib.Analysis.InnerProductSpace.PiL2
 /-!
 # Euclidean Plane
 
-This file defines the Euclidean Plane as a inner product real vector space equipped with a basis.
+This file defines the Euclidean Plane as an affine space, admits an action of an inner product real vector space of dimension two equipped with a basis.
 
 ## Important definitions
 
@@ -15,24 +15,56 @@ This file defines the Euclidean Plane as a inner product real vector space equip
 
 We introduce the notation `x(p)`, `y(p)` for the x,y coordinates of a point `p` on a Euclidean Plane.
 
+## Implementation Notes
+
+## Further Works
+
 -/
 
 
 /- Define Euclidean plane as normed vector space over ℝ of dimension 2 -/
 namespace EuclidGeom
 
-class EuclideanPlane (V : Type _)  extends  NormedAddCommGroup V, InnerProductSpace ℝ V where
+section Vectors
+
+class EuclideanVectorSpace (V : Type _)  extends  NormedAddCommGroup V, InnerProductSpace ℝ V where
   dim_two : FiniteDimensional.finrank ℝ V = 2
   basis : Basis (Fin 2) ℝ V
 
-variable {V : Type _} [h : EuclideanPlane V] (p : V) 
+variable {V : Type _} [h : EuclideanVectorSpace V]
 
-def x_coordinate := (Basis.coord h.basis 0).toFun
+def x_coordinate (v : V) := (Basis.coord h.basis 0).toFun v
 
-def y_coordinate := (Basis.coord h.basis 1).toFun
+def y_coordinate (v : V) := (Basis.coord h.basis 1).toFun v
 
-notation "x("p")" => x_coordinate p
-notation "y("p")" => y_coordinate p
+notation "x("v")" => x_coordinate v
+notation "y("v")" => y_coordinate v
+
+theorem eq_of_coord_eq {v₁ : V} {v₂ : V} (hx : x(v₁) = x(v₂)) (hy : y(v₁) = y(v₂)) : v₁ = v₂ := by 
+  rw [Basis.ext_elem_iff h.basis, Fin.forall_iff]
+  intro i hi
+  cases i with 
+  | zero => exact hx  
+  | succ i' => 
+    cases i' with 
+    | zero => exact hy
+    | succ => linarith
+
+end Vectors
+
+section Plane
+
+class EuclideanPlane (H : Type _) extends MetricSpace H where 
+  V : Type _
+  VectorSpace : EuclideanVectorSpace V
+  toNormedAddTorsor : NormedAddTorsor V H
+
+end Plane
+
+instance EuclideanVectorSpace.toEuclideanPlane (V : Type _) [h : EuclideanVectorSpace V] : EuclideanPlane V where
+  V := V
+  VectorSpace := h
+  toNormedAddTorsor := by infer_instance
 
 end EuclidGeom
 
