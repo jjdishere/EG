d62e35d02eb30d7f57a244d519d75cd62b87a963
Liang-Xiao-pku <xiaoliangmit@gmail.com>
2023-09-02 16:26:55 +0800

    9/2 16:26

--- EuclideanGeometry/Foundation/Axiom/Transport.lean
+++
@@ -1,8 +0,0 @@
-import EuclideanGeometry.Foundation.Axiom.Ray
-import EuclideanGeometry.Foundation.Axiom.Ray_ex1
-
-/- The purpose of of this file is to establish results about parallel transport along a vector. Presumably, all results are "invariant" under parallel transport. -/
-
-namespace EuclidGeom
-
-variable {P: Type _} [EuclideanPlane P] (v : ℝ × ℝ) (gseg : GDirSeg P) (seg : DirSeg P) (ray : Ray P)
