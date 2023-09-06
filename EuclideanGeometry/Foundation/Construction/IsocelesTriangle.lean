19d5719b2f0885a0c5e1b9dd9fccbfe42d79be35
Liang-Xiao-pku <xiaoliangmit@gmail.com>
2023-09-06 14:01:18 +0800

    Update IsocelesTriangle.lean

--- EuclideanGeometry/Foundation/Construction/IsocelesTriangle.lean
+++ EuclideanGeometry/Foundation/Construction/IsocelesTriangle.lean
@@ -11,6 +11,8 @@ namespace Triangle
 def IsIsocTri (tr : Triangle P) : Prop := tr.edge₂ = tr.edge₃
 
 theorem IsIsocTri_iff_two_angles_eq_of_nontriv_triangle (tr : Triangle P) (nontriv : tr.is_nontriv) : tr.IsIsocTri ↔ (tr.angle₂ nontriv = - tr.angle₃ nontriv) := sorry
+-- To show angle equal => sides equal, use anti-congruent of the triangle with itself.
+
 
 end Triangle
 
