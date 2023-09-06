10bad54841bddcb6a16f315921dcf080acfc1931
Liang-Xiao-pku <xiaoliangmit@gmail.com>
2023-09-06 13:59:52 +0800

    9/6 minor

--- EuclideanGeometry/Foundation/Axiom/Line.lean
+++ EuclideanGeometry/Foundation/Axiom/Line.lean
@@ -13,7 +13,9 @@ namespace Line
 
 variable  {P : Type _} [EuclideanPlane P] 
 
-def mk_pt_pt (A B : P) (h : B ≠ A) : Line P := sorry --careful here! should I use B ≠ A or A ≠ B ?
+-- define a line from two points 
+
+def mk_pt_pt (A B : P) (h : B ≠ A) : Line P := sorry --careful here! should I use B ≠ A or A ≠ B ? 
 
 end Line
 
@@ -31,9 +33,29 @@ def IsOnLine (a : P) (l : Line P) : Prop :=
 
 scoped infix : 50 "LiesOnLine" => IsOnLine
 
+
+section Compaitiblity_of_coersions
+
+
+-- If a point lies on a ray, then it lies on the line associated to the ray.
+theorem lies_on_line_of_ray_of_lies_on_ray (a : P) (l : Ray P) (h : a LiesOnRay l) : a LiesOnLine l := sorry
+
+-- If A and B are two distinct points, they lie on the line AB
+theorem source_or_ray_lies_on_line_of_ray (l : Ray P) : l.source LiesOnLine l := sorry
+
+theorem pt_lies_on_line_of_pt_pt (A B : P) (h: B ≠ A) : A LiesOnLine LIN A B h ∧ B LiesOnLine LIN A B h := sorry
+
+
+-- The line defined from two distinct points is equal to the line defined from the ray associated to two distinct points
+
+theorem line_eq_line_of_ray_of_pt_pt {A B : P} (h : B ≠ A) : LIN A B h = ((RAY A B h) : Line P) := sorry
+-- `not clear whether I should remove : Line P (it compiles)`
+
 theorem line_eq_line_of_pt_pt {A B : P} {l : Line P} (h : B ≠ A) (hA : A LiesOnLine l) (hB : B LiesOnLine l) : l = Line.mk_pt_pt A B h := sorry -- p ≠ q or q ≠ p ?
 
 theorem lieson_of_colinear_ne {A B C : P} (c : colinear A B C) (h : B ≠ A) : C LiesOnLine LIN A B h := sorry
+
+end Compaitiblity_of_coersions
 
 section Archimedean_property
 -- there are two distinct points on a line
---
+++ EuclideanGeometry/Foundation/Axiom/Position_ex.lean
@@ -0,0 +1,16 @@
+import EuclideanGeometry.Foundation.Axiom.Position
+import EuclideanGeometry.Foundation.Axiom.Ray_ex
+
+/- This file discuss the relative positions of points and rays on a plane. -/
+noncomputable section
+namespace EuclidGeom
+
+
+variable {P : Type _} [EuclideanPlane P] 
+
+
+/- Position of three (distinct) points.  Giving to colinear (futher classification) -/
+-- `see line_ex`
+
+
+end EuclidGeom
