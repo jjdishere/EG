c7b2c13eaf80a917a3853b2555df7ca68445f47a
Liang-Xiao-pku <xiaoliangmit@gmail.com>
2023-09-08 17:59:25 +0800

    9/8 minor changes
    mostly to expand circle.lean

--- EuclideanGeometry/Foundation/Axiom/Circle.lean
+++ EuclideanGeometry/Foundation/Axiom/Circle.lean
@@ -28,11 +28,67 @@ scoped notation "CIR" => Circle.mk_pt_pt
 
 scoped notation "⨀" => Circle.mk_pt_pt
 
-
+section coersion
 
 def Triangle.toCir_of_nontriv (tr : Triangle P) (nontriv : tr.is_nontriv) : Circle P := sorry
 
--- point lieson, inside, outside a circle
+end coersion
+
+
+section position
+
+namespace Circle
+
+-- Define the power of a point P relative to a circle ω with center O and radius r to be OP ^ 2 - r ^ 2
+
+def power (ω : Circle P) (p : P) : ℝ := (SEG ω.center p).length ^ 2 - ω.radius ^ 2
+
+def lies_inside_circle (p : P) (ω : Circle P) : Prop := (SEG ω.center p).length ≤  ω.radius
+
+def lies_on_circle (p : P) (ω : Circle P) : Prop := (SEG ω.center p).length = ω.radius
+
+def lies_interior_circle (p : P) (ω : Circle P) : Prop := (SEG ω.center p).length < ω.radius
+
+def lies_outside_circle (p : P) (ω : Circle P) : Prop := ω.radius < (SEG ω.center p).length
+
+end Circle 
+
+scoped infix : 50 "LiesIntCir" => Circle.lies_interior_circle
+scoped infix : 50 "LiesInCir" => Circle.lies_inside_circle
+scoped infix : 50 "LiesOnCir" => Circle.lies_on_circle
+scoped infix : 50 "LiesOutCir" => Circle.lies_outside_circle
+
+namespace Circle
+
+
+theorem inside_circle_iff_power_neg (p : P) (ω : Circle P) : p LiesInCir ω ↔ ω.power p ≤  0 := sorry
+
+theorem interior_of_circle_iff_power_neg (p : P) (ω : Circle P) : p LiesIntCir ω ↔ ω.power p < 0 := sorry
+
+theorem lies_on_circle_iff_power_zero (p : P) (ω : Circle P) : p LiesOnCir ω ↔ ω.power p = 0 := sorry
+
+theorem outside_circle_iff_power_pos (p : P) (ω : Circle P) : p LiesOutCir ω ↔ 0 < ω.power p  := sorry
+
+theorem interior_of_circle_iff_inside_not_on_circle (p : P) (ω : Circle P) : p LiesIntCir ω ↔ p LiesInCir ω ∧ (¬ p LiesOnCir ω) := sorry
+
+-- Define a concept of segment to be entirely contained in a circle, to mean that the two endpoints of a segment to lie inside a circle.
+
+def seg_lies_inside_circle (l : Seg P) (ω : Circle P) : Prop := l.source LiesInCir ω ∧ l.target LiesInCir ω
+
+end Circle
+
+scoped infix : 50 "SegInCir" => Circle.seg_lies_inside_circle
+
+namespace Circle
+
+-- very hard question: if a segment lies inside a circle, then the interior point of a
+
+theorem pt_lies_inside_circle_of_seg_inside_circle {l : Seg P} {ω : Circle P} (h : l SegInCir ω) {p : P} (lieson : p LiesOnIntSeg l) : p LiesIntCir ω := sorry
+
+end Circle
+
+end position
+
 -- ray with circle
 -- line with circle
 end EuclidGeom
