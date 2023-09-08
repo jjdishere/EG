3809bacc0ac7c9ae5bfc2e05692054853445399c
cybcatppuccino <1538580589@qq.com>
2023-09-08 23:58:41 +0800

    Update Vector.lean
    small changes

--- EuclideanGeometry/Foundation/Axiom/Vector.lean
+++ EuclideanGeometry/Foundation/Axiom/Vector.lean
@@ -58,9 +58,7 @@ protected def AddGroupNorm : AddGroupNorm (ℝ × ℝ) where
         simp [pow_two, add_mul, mul_add]
         let h1 := two_mul_le_add_pow_two (x.1 * y.2) (x.2 * y.1)
         linarith
-    let Q := P
     rw [pow_two] at P
-    rw [mul_comm (Real.sqrt (x.fst ^ 2 + x.snd ^ 2)) _] at Q
     simp [pow_two] at *    
     linarith
 
@@ -410,8 +408,6 @@ theorem neg_normalize_eq_normalize_smul_neg {u v : ℝ × ℝ} (hu : u ≠ 0) (h
   rw [smul_algebra_smul_comm _ _ _] at g₁
   rw [neg_eq_iff_eq_neg, ← neg_smul _ _, ← inv_neg, ← Iff.mpr (inv_smul_eq_iff₀ (Iff.mpr neg_ne_zero (Iff.mpr (@norm_ne_zero_iff _ Vec.NormedAddGroup _) hv))) (Eq.symm g₁)]
 
--- main theorem of toProj
-
 theorem eq_toProj_of_smul {u v : ℝ × ℝ} (hu : u ≠ 0) (hv : v ≠ 0) {t : ℝ} (h : v = t • u) : Vec.toProj_of_nonzero u hu = Vec.toProj_of_nonzero v hv := by
   have ht : t ≠ 0 := by
     by_contra ht'
@@ -452,4 +448,16 @@ theorem smul_of_eq_toProj {u v : ℝ × ℝ} {hu : u ≠ 0} {hv : v ≠ 0} (h : 
       rw [← neg_inv, neg_smul]
       exact h₂
 
+-- The main theorem of toProj
+
+theorem eq_toProj_iff {u v : ℝ × ℝ} (hu : u ≠ 0) (hv : v ≠ 0) : (Vec.toProj_of_nonzero u hu = Vec.toProj_of_nonzero v hv) ↔ ∃ (t : ℝ), v = t • u := by
+  constructor
+  intro h
+  exact smul_of_eq_toProj h
+  intro h'
+  rcases h' with ⟨t, h⟩ 
+  exact eq_toProj_of_smul hu hv h
 end EuclidGeom
+
+-- Define two Proj is perpendicular by the mul structure of ℂ, using Complex.I
+
