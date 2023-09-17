9f338bd36cfea400c534a6dae1d0f9c04a29595f
hkskcyiv <1272427896@qq.com>
2023-09-17 15:19:57 +0800

    proved some theorems

--- EuclideanGeometry/Foundation/Axiom/Linear/Perpendicular.lean
+++ EuclideanGeometry/Foundation/Axiom/Linear/Perpendicular.lean
@@ -10,28 +10,6 @@ def perpendicular (l₁ l₂: LinearObj P) : Prop := l₁.toProj = l₂.toProj.p
 scoped infix : 50 "IsPerpendicularTo" => perpendicular
 scoped infix : 50 "⟂" => perpendicular
 
-theorem Proj.I_ne_one : ¬1=(Proj.I) := by
-  intro h
-  have h0: Dir.I=1∨Dir.I=-1 := by
-    apply (Con.eq PM.con).mp 
-    exact id (Eq.symm h)
-  have h1: (Dir.I)^2=1 := by
-    rcases h0 with h2|h2
-    rw[h2]
-    exact one_pow 2
-    rw[h2]
-    exact neg_one_sq
-  have h3: (Dir.I)^2=-1 :=by
-    rw[←Dir.I_mul_I_eq_neg_one]
-    exact sq Dir.I
-  have h4: ¬(-1:Dir)=(1:Dir) := by
-   intro k 
-   rw [Dir.ext_iff, Prod.ext_iff] at k
-   simp at k
-   linarith
-  rw[h3] at h1
-  exact h4 h1
-
 namespace perpendicular
 
 @[simp]
@@ -42,7 +20,7 @@ protected theorem irrefl (l : LinearObj P)  : ¬ (l ⟂ l) := by
   have h0: 1=Proj.I := by
     nth_rw 1 [←one_mul (LinearObj.toProj l)] at h
     exact mul_right_cancel h
-  exact Proj.I_ne_one h0
+  exact Proj.one_ne_I h0
 
 protected theorem symm (l₁ l₂ : LinearObj P) : (l₁ ⟂ l₂) → (l₂ ⟂ l₁) := sorry
 
@@ -76,7 +54,7 @@ theorem toProj_ne_toProj_of_perp (l₁ l₂: LinearObj P) : (l₁ ⟂ l₂) → 
   have h2 : 1=Proj.I:= by
     nth_rw 1 [←one_mul (LinearObj.toProj l₂)] at h0
     exact mul_right_cancel h0
-  exact Proj.I_ne_one h2
+  exact Proj.one_ne_I h2
 
 end Perpendicular_and_parallel
 
@@ -89,7 +67,13 @@ theorem toProj_of_perp_line_eq_toProj_perp (A : P) (l : Line P) : (perp_line A l
   sorry
 
 theorem perp_foot_preparation (A : P) (l : Line P) : l.toProj ≠ (perp_line A l).toProj := by
-  sorry
+  rw[toProj_of_perp_line_eq_toProj_perp]
+  unfold Proj.perp
+  by_contra h0
+  have h1 : 1=Proj.I:= by
+    nth_rw 1 [←one_mul (Line.toProj l)] at h0
+    exact mul_right_cancel h0
+  exact Proj.one_ne_I h1
 
 def perp_foot (A : P) (l : Line P) : P := intersection_of_nonparallel_line l (perp_line A l) (perp_foot_preparation A l)
 
