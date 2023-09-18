add33451aecbaf6696f3b82131ad032dc356321a
blackbox67 <91403106+HeRunming@users.noreply.github.com>
2023-09-18 16:20:27 +0800

    Update Ray_ex.lean

--- EuclideanGeometry/Foundation/Axiom/Linear/Ray_ex.lean
+++ EuclideanGeometry/Foundation/Axiom/Linear/Ray_ex.lean
@@ -18,12 +18,19 @@ def Seg.reverse (seg : Seg P): Seg P where
   target := seg.source
 
 -- reversing the toDir does not change the nontriviality of a generalized directed segment.
-theorem nd_of_rev_of_nd {seg : Seg P} (nd : seg.is_nd) : seg.reverse.is_nd := sorry
+theorem nd_of_rev_of_nd {seg : Seg P} (nd : seg.is_nd) : seg.reverse.is_nd := by
+-- sorry
+  simp [Seg.is_nd]
+  push_neg
+  symm
+  apply nd
 
 def Seg_nd.reverse : Seg_nd P := ⟨seg_nd.1.reverse, nd_of_rev_of_nd seg_nd.2⟩ 
 
 -- double reverse a ray gets back to itself
-theorem Ray.rev_rev_eq_self : ray.reverse.reverse = ray := sorry
+theorem Ray.rev_rev_eq_self : ray.reverse.reverse = ray := by
+-- sorry
+  simp [reverse]
 
 -- double reverse a generalized directed segment gets back to itself
 theorem Seg.rev_rev_eq_self  : seg.reverse.reverse = seg := rfl
@@ -33,20 +40,107 @@ theorem Seg_nd.rev_rev_eq_self  : seg_nd.reverse.reverse = seg_nd := rfl
 theorem Seg_nd.rev_toSeg_eq_toSeg_rev : seg_nd.1.reverse = seg_nd.reverse.1 := rfl
 
 -- reversing the toDir does not change the property that a point lies on the generalized directed segments.
-theorem Seg.lies_on_rev_iff_lie_son {A : P} (lieson : A LiesOn seg) : A LiesOn seg.reverse := sorry
+theorem Seg.lies_on_rev_iff_lie_son {A : P} (lieson : A LiesOn seg) : A LiesOn seg.reverse := by
+-- sorry
+  unfold lies_on Carrier.carrier instCarrierSeg
+  simp only [Set.setOf_mem_eq]
+  rcases lieson with ⟨a,⟨h1,⟨h2,h3⟩⟩⟩
+  use 1-a
+  constructor
+  linarith
+  constructor
+  linarith
+  simp only [reverse]
+  rw[sub_smul,sub_eq_add_neg]
+  simp
+  have h:-(a • VEC seg.target seg.source)=a • (-1) • VEC seg.target seg.source:=by
+    rw[smul_algebra_smul_comm]
+    rw[neg_smul]
+    simp
+  have h':VEC seg.target A=VEC seg.target seg.source+VEC seg.source A:=by
+    rw[←neg_vec seg.source seg.target,add_comm,←sub_eq_add_neg,vec_sub_vec]
+  rw[h,h',←neg_vec seg.source seg.target]
+  simp
+  exact h3
+  
+theorem eq_source_iff_lies_on_ray_lies_on_ray_rev {A : P} : A = ray.source ↔ (A LiesOn ray) ∧ (A LiesOn ray.reverse) := by
+-- sorry
+  constructor
+  intro h
+  constructor
+  use 0
+  simp
+  rw[h,vec_same_eq_zero]
+  use 0
+  simp
+  simp only [Ray.reverse]
+  rw[h,vec_same_eq_zero]
+  simp
+  rintro ⟨a,⟨anneg,h⟩⟩ ⟨b,⟨bnneg,h'⟩⟩
+  simp only [Ray.reverse] at h'
+  simp at h'
+  rw[h,←add_zero a,← sub_self b,add_sub,sub_smul] at h'
+  simp at h'
+  have h'': a+b=0:=by
+    contrapose! h'
+    constructor
+    exact h'
+    apply Dir.toVec_ne_zero
+  have:a=0:=by
+    linarith
+  rw[this] at h
+  simp at h
+  rw[eq_iff_vec_eq_zero,h]
 
-theorem eq_source_iff_lies_on_ray_lies_on_ray_rev {A : P} : A = ray.source ↔ (A LiesOn ray) ∧ (A LiesOn ray.reverse) := sorry
+theorem not_lies_on_of_lies_int_rev {A : P} (liesint : A LiesInt ray.reverse) : ¬ A LiesOn ray := by
+-- sorry
+  by_contra h
+  rcases liesint with ⟨h',nsource⟩
+  have: A LiesOn ray.reverse:=by
+    apply h'
+  have :A=ray.source:=by
+    rw[eq_source_iff_lies_on_ray_lies_on_ray_rev]
+    constructor
+    exact h
+    exact this
+  trivial
 
-theorem not_lies_on_of_lies_int_rev {A : P} (liesint : A LiesInt ray.reverse) : ¬ A LiesOn ray := sorry
-
-theorem not_lies_int_of_lies_on_rev {A : P} (liesint : A LiesOn ray.reverse) : ¬ A LiesInt ray := sorry
-
--- A point lies on the directed segment if and only if it lies on the ray associated to the segment and the ray associated to the reverse of this segment.
-theorem lies_on_iff_lies_on_toRay_and_rev_toRay {A : P} : A LiesOn seg_nd.1 ↔ (A LiesOn seg_nd.toRay) ∧ (A LiesOn seg_nd.reverse.toRay) := sorry
+theorem not_lies_int_of_lies_on_rev {A : P} (liesint : A LiesOn ray.reverse) : ¬ A LiesInt ray :=by
+--  sorry
+  by_contra h
+  rw[←Ray.rev_rev_eq_self ray] at h
+  have:¬A LiesOn ray.reverse:=by
+    apply not_lies_on_of_lies_int_rev 
+    exact h
+  trivial
 
 theorem Ray.toDir_of_reverse_eq_neg_toDir : ray.reverse.toDir = - ray.toDir := rfl
 
-theorem Ray.toProj_of_reverse_eq_toProj : ray.reverse.toProj = ray.toProj := sorry
+theorem Ray.toProj_of_reverse_eq_toProj : ray.reverse.toProj = ray.toProj := by
+--sorry
+--@HeRunming: Simply imitate the proof of theorem "eq_toProj_of_smul" in Vector.lean
+  unfold Ray.reverse
+  unfold Ray.toDir
+  unfold Ray.toProj
+  apply Quotient.sound
+  unfold HasEquiv.Equiv instHasEquiv PM.con PM
+  simp only [Con.rel_eq_coe, Con.rel_mk]
+  unfold EuclidGeom.Ray.toDir
+  right
+  rfl
+
+  
+
+
+  
+
+
+  
+  
+
+  
+  
+
 
 theorem Seg.toVec_of_reverse_eq_neg_toVec : seg.reverse.toVec = - seg.toVec := sorry
 
@@ -59,6 +153,91 @@ theorem Seg_nd.toProj_of_reverse_eq_toProj : seg_nd.reverse.toProj = seg_nd.toPr
 -- reversing the toDir does not change the length
 theorem length_eq_length_of_rev : seg.length = seg.reverse.length := sorry
 
+-- A point lies on the directed segment if and only if it lies on the ray associated to the segment and the ray associated to the reverse of this segment.
+-- need to be refined
+theorem lies_on_iff_lies_on_toRay_and_rev_toRay {A : P} : A LiesOn seg_nd.1 ↔ (A LiesOn seg_nd.toRay) ∧ (A LiesOn seg_nd.reverse.toRay) := by
+-- sorry
+  constructor
+  intro liesonseg
+  constructor
+  apply Seg_nd.lies_on_toRay_of_lies_on
+  trivial
+  apply Seg_nd.lies_on_toRay_of_lies_on
+  apply Seg.lies_on_rev_iff_lie_son
+  trivial
+  rintro ⟨⟨a,anneg,h⟩,b,bnneg,h'⟩
+  unfold Dir.toVec Ray.toDir Seg_nd.toRay at h h'
+  simp at h h'
+  rw[Seg_nd.toDir_of_reverse_eq_neg_toDir] at h'
+  have tria:(-Seg_nd.toDir seg_nd).1=(-1)•(Seg_nd.toDir seg_nd).1:=by
+    rw[Dir.toVec_neg_eq_neg_toVec _]
+    rw[neg_one_smul]
+  unfold Dir.toVec at tria
+  rw[tria] at h'
+  simp only [Seg_nd.toDir] at h h'
+  have trib:b • -1 • (Vec_nd.normalize (Seg_nd.toVec_nd seg_nd)).1=(-b)• (Vec_nd.normalize (Seg_nd.toVec_nd seg_nd)).1:=by
+    simp
+  unfold Dir.toVec at trib
+  rw[trib] at h'
+  set v:=(Vec_nd.normalize (Seg_nd.toVec_nd seg_nd)).1 with v_def
+  unfold Dir.toVec at v_def
+  simp only [Seg_nd.reverse,Seg.reverse] at h'
+  rw[←v_def] at h h'
+  have asumbv:(a+b)•v=VEC seg_nd.1.source seg_nd.1.target:=by
+    rw[← vec_add_vec _ A _,←neg_vec seg_nd.1.target A,h,h',add_smul]
+    simp
+  have tri1:VEC seg_nd.1.source seg_nd.1.target=seg_nd.toVec_nd.1:=by
+    rfl
+  simp at asumbv
+  rw[tri1,←Vec_nd.norm_smul_normalize_eq_self seg_nd.toVec_nd] at asumbv
+  have tri2:(a+b-(Vec.norm seg_nd.toVec_nd))•(seg_nd.toDir).1=0:=by
+    rw[sub_smul,asumbv]
+    simp
+  have asumb:a+b=(Vec.norm seg_nd.toVec_nd):=by
+    have asumb:a+b-(Vec.norm seg_nd.toVec_nd)=0:=by
+      rcases smul_eq_zero.mp tri2 with hyp1|hyp2
+      exact hyp1
+      exfalso
+      apply Dir.toVec_ne_zero seg_nd.toDir
+      assumption
+    linarith
+  
+  have norm_pos_vec:0<Vec.norm seg_nd.toVec_nd:=by
+    rw[Vec.norm_eq_abs_toComplex]
+    unfold Complex.abs
+    simp
+    push_neg
+    apply ne_zero_of_Vec_nd_toComplex
+  have norm_nonzero:Vec.norm seg_nd.toVec_nd≠0:=by
+    linarith
+  have alenorm:a≤Vec.norm seg_nd.toVec_nd:=by
+    linarith
+  have tri3:1=Vec.norm seg_nd.toVec_nd*(Vec.norm seg_nd.toVec_nd)⁻¹:=by
+    rw[mul_inv_cancel norm_nonzero]
+  use a*(Vec.norm seg_nd.toVec_nd)⁻¹
+  constructor
+  apply mul_nonneg
+  exact anneg
+  simp
+  linarith
+  constructor
+  rw[tri3]
+  apply mul_le_mul
+  exact alenorm
+  trivial
+  simp
+  apply le_trans anneg
+  exact alenorm
+  apply le_trans anneg
+  exact alenorm
+  rw[h,mul_smul,tri1]
+  nth_rw 2[←Vec_nd.norm_smul_normalize_eq_self]
+  simp only [v_def]
+  rw[smul_smul,smul_smul,mul_assoc,←smul_smul]
+  rw[mul_comm] at tri3
+  rw[←tri3]
+  simp
+  
 end reverse
 
 section extension
