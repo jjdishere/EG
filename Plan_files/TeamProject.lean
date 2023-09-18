3c683c5e5b47ef9a0fc6b46cf12da94416490c2f
jjdishere <emailboxofjjd@163.com>
2023-09-18 20:43:51 +0800

    Update TeamProject.lean
    Add name of team leader

--- Plan_files/TeamProject.lean
+++ Plan_files/TeamProject.lean
@@ -13,22 +13,30 @@ This file describes the projects of each team.
 4. Currently, the whole design of this Euclidean Geometry project is still unstable, even for the very foundation files. It is possible to reformulate and reprove many currently existing theorems under the new design. As a result, building your proof dependent on fancier, later theorems is much better than building your proof on very elemental theorems (such as unfold everything). This is so called `de-couple`. It minimize the effort of implementing new designs, changing one place will not raise errors everywhere. We kindly ask you to follow this principle.
 
 ## Project Detail
-Team D 1 : Fills sorry's in Plane.lean, section length, and section existence in Ray.lean.
+Team D 1 Muqing Jiang : 
+* Fills sorry's in Plane.lean, section length, and section existence in Ray.lean.
 
-Team A 2 : Fills sorry's in section coersion_compatibility and section midpoint Ray.lean
+Team A 2 Yongle Bai :
+* Fills sorry's in section coersion_compatibility and section midpoint Ray.lean
 
-Team F 3 : Fills sorry's in Ray_ex.lean `Near Finished` `More to be assigned`
+Team F 3 Zhuoni Chi :
+* Fills sorry's in Ray_ex.lean `Near Finished` `More to be assigned`
 
-Team C 4 : Fills sorry's in Line.lean
+Team C 4 Xintao Yu :
+* Fills sorry's in Line.lean
 
-Team E 5 : Fills sorry's in Line_ex.lean
+Team E 5 Haoran Wang :
+* Fills sorry's in Line_ex.lean
 
-Team H 6 : Fills sorry's in Perpendicular.lean `Finished`
-Fills sorry's in cosine theorem and prove SAS in congreunce .lean 
+Team H 6 Yebo Peng :
+* Fills sorry's in Perpendicular.lean `Finished`
+* Fills sorry's in cosine theorem and prove SAS in congreunce .lean 
 
-Team G 7 : Fills sorry's in Congruence.lean, but you may leave SAS blank and use SAS (and cosine theorem, sine theorem) to show other triangle congruence criteria.
+Team G 7 Zhaohui Zong :
+* Fills sorry's in Congruence.lean, but you may leave SAS blank and use SAS (and cosine theorem, sine theorem) to show other triangle congruence criteria.
 
-Team B 8 : Fills sorry's in IsocelesTriangle.lean
+Team B 8 Zhehao Zheng :
+* Fills sorry's in IsocelesTriangle.lean
 
 ## Special Notes
 
