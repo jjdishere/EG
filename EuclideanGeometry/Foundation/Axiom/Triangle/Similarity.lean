a1ba889dc2712cec13370bb80ac6f813ccf6591e
jjdishere <emailboxofjjd@163.com>
2023-09-18 16:10:04 +0800

    Update Similarity.lean

--- EuclideanGeometry/Foundation/Axiom/Triangle/Similarity.lean
+++ EuclideanGeometry/Foundation/Axiom/Triangle/Similarity.lean
@@ -33,4 +33,26 @@ theorem ratio₃ : tr₁.1.edge₁.length / tr₁.1.edge₂.length = tr₂.1.edg
 
 end IsSim
 
+namespace IsASim
+
+variable (tr₁ tr₂ : Triangle_nd P) (h : tr₁ ∼ₐ tr₂)
+
+/- If $tr_1 ∼ tr_2$, then ... -/
+theorem ratio₁ : tr₁.1.edge₂.length / tr₁.1.edge₃.length = tr₂.1.edge₂.length / tr₂.1.edge₃.length := sorry
+
+theorem ratio₂ : tr₁.1.edge₃.length / tr₁.1.edge₁.length = tr₂.1.edge₃.length / tr₂.1.edge₁.length := sorry
+
+theorem ratio₃ : tr₁.1.edge₁.length / tr₁.1.edge₂.length = tr₂.1.edge₁.length / tr₂.1.edge₂.length := sorry
+
+end IsASim
+
+section simiarity_criterion
+
+/- AA -/
+theorem sim_of_AA (tr₁ tr₂ : Triangle_nd P) (h₂ : tr₁.oangle₂.value = tr₂.oangle₂.value) (h₃ : tr₁.oangle₃.value = tr₂.oangle₃.value) : tr₁ ∼ tr₂ := sorry 
+
+theorem asim_of_AA (tr₁ tr₂ : Triangle_nd P) (h₂ : tr₁.oangle₂.value = - tr₂.oangle₂.value) (h₃ : tr₁.oangle₃.value = - tr₂.oangle₃.value) : tr₁ ∼ₐ tr₂ := sorry
+
+end simiarity_criterion
+
 end EuclidGeom
