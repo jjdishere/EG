b259d75efb45c2ad3214d53642e581c12cad20af
Liang-Xiao-pku <xiaoliangmit@gmail.com>
2023-09-19 14:26:34 +0800

    Update Angle_ex2.lean

--- EuclideanGeometry/Foundation/Axiom/Position/Angle_ex2.lean
+++ EuclideanGeometry/Foundation/Axiom/Position/Angle_ex2.lean
@@ -8,7 +8,25 @@ namespace EuclidGeom
 
 variable {P : Type _} [EuclideanPlane P]
 
+section OAngleSum
+
+namespace OAngle
+
+theorem source_start_ray_eq_source_end_ray (oang : OAngle P) : oang.start_ray.source = oang.end_ray.source := sorry
+
+
+-- Can use congrArg @Ray.source P _) h to turn h into the sources of two terms being equal.
+theorem source_eq_source_of_adj {oang₁ oang₂: OAngle P} (h :oang₂.start_ray = oang₁.end_ray) : oang₁.source = oang₂.source := sorry
+
+
+def sum_adj (oang₁ oang₂: OAngle P) (h :oang₂.start_ray = oang₁.end_ray) : OAngle P := OAngle.mk oang₁.start_ray oang₂.end_ray (source_eq_source_of_adj h)
+
+theorem ang_eq_ang_add_ang_mod_pi_of_adj_oang (oang₁ oang₂ oang₃: OAngle P) (h: oang₁.end_ray = oang₂.start_ray)
+
 /- Theorems such as if two adjacent angle add up to π, then the other ray form a line. -/
 
+end OAngle 
+
+section OAngleSum
 
 end EuclidGeom
