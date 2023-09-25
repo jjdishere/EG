2daa90856665d412dc199932f1991cf0db2ab88e
stupidchunchun <144506563+stupidchunchun@users.noreply.github.com>
2023-09-25 20:26:07 +0800

    Team B ShanZun ex. ; repair IsIsoceles
    1. 完成了Example.ShanZun-Ex1.lean 里的一个几何题的形式化
    2. 按照上次pr的review的要求简化了Construction.Triangle.IsocelesTriangle的证明
    3. 把加在Axiom.Triangle.Basic_ex中的几个定理细化了一下

--- EuclideanGeometry/Example/ShanZun-Ex1.lean
+++ EuclideanGeometry/Example/ShanZun-Ex1.lean
@@ -1,4 +1,6 @@
 import EuclideanGeometry.Foundation.Index
+import EuclideanGeometry.Foundation.Axiom.Linear.Line_ex
+import EuclideanGeometry.Foundation.Axiom.Linear.Ray
 
 noncomputable section
 
@@ -19,22 +21,50 @@ variable {A B C : P} {hnd : ¬ colinear A B C}
 
 end Shan_Problem_1_1
 
-namespace Shan_Problem_1_2
-/- Let $\triangle ABC$ be a nondegenerate isosceles triangle in which $AB = AC$. Let $D$ be a point on the extension of $AB$ and $E$ a point on the extension of $AC$, such that $\angle EBC = \angle BCD$.
 
+
+namespace Shan_Problem_1_2
+/- Let $\triangle ABC$ be a nondegenerate isosceles triangle in which $AB = AC$. 
+Let $D$ be a point on the extension of $AB$ and $E$ a point on the extension of $AC$,
+such that $\angle EBC = \angle BCD$.
 Prove that $\angle CDA = \angle BEA$. -/
+
 variable {A B C : P} {hnd : ¬ colinear A B C} {hisoc : (▵ A B C).IsIsoceles}
--- Claim: $A \ne B$ and $A \ne C$ and $B \ne C$. This is because vertices of nondegenerate triangles are distinct.
-lemma a_ne_b : A ≠ B := (ne_of_not_colinear hnd).2.2.symm
-lemma a_ne_c : A ≠ C := (ne_of_not_colinear hnd).2.1
+-- Claim: $A \ne B$ and $A \ne C$ and $B \ne C$.
+--This is because vertices of nondegenerate triangles are distinct.
+lemma b_ne_a : B ≠ A := (ne_of_not_colinear hnd).2.2
+lemma c_ne_a : C ≠ A := (ne_of_not_colinear hnd).2.1.symm
 lemma b_ne_c : B ≠ C := (ne_of_not_colinear hnd).1.symm
 -- Let $D$ and $e$ be points on the extension of the nondegenerate segments of $AB$ and $AC$, respectively.
-variable {D E : P} {hd : Ray.IsInt D (SEG_nd A B a_ne_b).extension} {he : Ray.IsInt E (SEG_nd A C a_ne_c).extension}
--- Claim: $E \ne B$ and $D \ne C$. This is because 
---lemma extn.source_eq_target : {seg_nd : Seg_nd P} seg_nd.1.target := sorry
---lemma e_ne_b : E ≠ B := by
+variable {D E : P} {hd : D LiesOn (SEG_nd A B (b_ne_a (hnd := hnd))).extension} 
+{he : E LiesOn (SEG_nd A C (c_ne_a (hnd := hnd))).extension}
+-- Claim: $E \ne B$ and $D \ne C$. This is because $E$ lies on line AC, but $B$ doesn't lies on AC; $D$ lies on line AB, but $C$ doesn't lies on AB. 
+lemma e_ne_b : E ≠ B := ne_of_lieson_and_not_lieson (Seg_nd.lies_on_toline_of_lies_on_extn he) ((lies_on_iff_colinear_of_ne_lies_on_lies_on (c_ne_a (hnd := hnd)) (Seg_nd.lies_on_toLine_of_lie_on (Seg.source_lies_on (SEG_nd A C (c_ne_a (hnd := hnd))).1)) (Seg_nd.lies_on_toLine_of_lie_on (Seg.target_lies_on (SEG_nd A C (c_ne_a (hnd := hnd))).1)) B).mp.mt (flip_colinear_snd_trd.mt hnd))
+  --have b_notlieson_ac : ¬ B LiesOn (SEG_nd A C (c_ne_a (hnd := hnd))).toLine := (lies_on_iff_colinear_of_ne_lies_on_lies_on (c_ne_a (hnd := hnd)) (Seg_nd.lies_on_toLine_of_lie_on (Seg.source_lies_on (SEG_nd A C (c_ne_a (hnd := hnd))).1)) (Seg_nd.lies_on_toLine_of_lie_on (Seg.target_lies_on (SEG_nd A C (c_ne_a (hnd := hnd))).1)) B).mp.mt (flip_colinear_snd_trd.mt hnd)
+  --have e_lieson_ac : E LiesOn (SEG_nd A C (c_ne_a (hnd := hnd))).toLine := Seg_nd.lies_on_toline_of_lies_on_extn he
+lemma d_ne_c : D ≠ C := ne_of_lieson_and_not_lieson (Seg_nd.lies_on_toline_of_lies_on_extn hd) ((lies_on_iff_colinear_of_ne_lies_on_lies_on (b_ne_a (hnd := hnd)) (Seg_nd.lies_on_toLine_of_lie_on (Seg.source_lies_on (SEG_nd A B (b_ne_a (hnd := hnd))).1)) (Seg_nd.lies_on_toLine_of_lie_on (Seg.target_lies_on (SEG_nd A B (b_ne_a (hnd := hnd))).1)) C).mp.mt hnd)
+  --have c_notlieson_ab := (lies_on_iff_colinear_of_ne_lies_on_lies_on (b_ne_a (hnd := hnd)) (Seg_nd.lies_on_toLine_of_lie_on (Seg.source_lies_on (SEG_nd A B (b_ne_a (hnd := hnd))).1)) (Seg_nd.lies_on_toLine_of_lie_on (Seg.target_lies_on (SEG_nd A B (b_ne_a (hnd := hnd))).1)) C).mp.mt hnd
+  --have d_lieson_ab := Seg_nd.lies_on_toline_of_lies_on_extn hd
+-- Claim: $A \ne D$ and $A \ne E$. This is because $E$ lies on extension of $AC$, but $A$ doesn't lies on extension of $AC$; $D$ lies on extension of $AB$, but $A$ doesn't lies on extension of $AB$
+lemma a_ne_d : A ≠ D := by
+  have a_notlieson_ab_extn : ¬ A LiesOn (SEG_nd A B (b_ne_a (hnd := hnd))).extension := by
+    apply Ray.not_lies_on_of_lies_int_rev
+    simp only [extn_eq_rev_toray_rev, Ray.rev_rev_eq_self]
+    constructor
+    exact Seg_nd.lies_on_toRay_of_lies_on (SEG_nd A B (b_ne_a (hnd := hnd))).reverse A (Seg.target_lies_on (SEG_nd A B (b_ne_a (hnd := hnd))).reverse.1)
+    exact (b_ne_a (hnd := hnd)).symm
+  exact (ne_of_lieson_and_not_lieson hd a_notlieson_ab_extn).symm
+lemma a_ne_e : A ≠ E := by
+  have a_notlieson_ac_extn : ¬ A LiesOn (SEG_nd A C (c_ne_a (hnd := hnd))).extension := by
+    apply Ray.not_lies_on_of_lies_int_rev
+    simp only [extn_eq_rev_toray_rev, Ray.rev_rev_eq_self]
+    constructor
+    exact Seg_nd.lies_on_toRay_of_lies_on (SEG_nd A C (c_ne_a (hnd := hnd))).reverse A (Seg.target_lies_on (SEG_nd A C (c_ne_a (hnd := hnd))).reverse.1)
+    exact (c_ne_a (hnd := hnd)).symm
+  exact (ne_of_lieson_and_not_lieson he a_notlieson_ac_extn).symm
+-- -- We have $\angle EBC = \angle BCD$.
+variable (hang : ∠ E B C e_ne_b b_ne_c.symm =  ∠ B C D b_ne_c d_ne_c)
 
+theorem Shan_Problem_1_2 : ∠ C D A (d_ne_c (hnd := hnd) (hd := hd)).symm (a_ne_d (hnd := hnd) (hd := hd)) = ∠ B E A (e_ne_b (hnd := hnd) (he := he)).symm (a_ne_e (hnd := hnd) (he := he)) := sorry
 
--- We have $\angle EBC = \angle BCD$.
---variable {hang : }
 end Shan_Problem_1_2
--- EuclideanGeometry/Foundation/Construction/Triangle/IsocelesTriangle.lean
+++ EuclideanGeometry/Foundation/Construction/Triangle/IsocelesTriangle.lean
@@ -6,15 +6,6 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
 
 
--- 增加了edge_eq_edge_of_flip_vertices等四个关于 perm,flip的边,角相等的定理
--- Triangle.Basic判断clock的地方：clock_of_neg_angle 结论从is_cclock 改为 ¬ ... is_cclock
--- 判断三角形反全等的地方：acongr_of_AAS，acongr_of_ASA；的结论从全等改为反全等
-
--- 第一题is_isoceles_tri_iff_ang_eq_ang_of_nd_tri把angle₂=-angle₃ 删去负号
--- 最后三题regular_tri_of_isoceles_tri_of_fst_ang_eq_sixty_deg 等，加上了isisoceles 的条件
--- ang_eq_sixty_deg_of_cclock_regular_tri 加上了等边三角形的条件
-
-
 noncomputable section
 namespace EuclidGeom
 
@@ -30,32 +21,27 @@ def Triangle.IsIsoceles (tri : Triangle P) : Prop := tri.edge₂.length = tri.ed
 
 theorem is_isoceles_tri_iff_ang_eq_ang_of_nd_tri {tri_nd : Triangle_nd P} : tri_nd.1.IsIsoceles ↔ (tri_nd.angle₂.value=  tri_nd.angle₃.value) := by
 -- To show angle equal => sides equal, use anti-congruent of the triangle with itself. SAS
-rcases Triangle.edge_eq_edge_of_flip_vertices tri_nd.1 with ⟨a, b, c⟩
-rcases Triangle.angle_eq_neg_angle_of_flip_vertices tri_nd with ⟨x, y, z⟩ 
-have h21 : tri_nd.1.edge₃.length = tri_nd.flip_vertices.1.edge₂.length := c
-have h22 : tri_nd.1.edge₂.length = tri_nd.flip_vertices.1.edge₃.length := b
-have h23 : tri_nd.angle₁.value = - tri_nd.flip_vertices.angle₁.value := x
-have h24 : tri_nd.angle₃.value = - tri_nd.flip_vertices.angle₂.value := z
+rcases Triangle_nd.edge_eq_edge_of_flip_vertices tri_nd with ⟨a, b, c⟩
+rcases Triangle_nd.angle_eq_neg_angle_of_flip_vertices tri_nd with ⟨x, y, z⟩ 
 constructor
 intro h0
 have h1 : tri_nd.1.edge₂.length = tri_nd.1.edge₃.length := by
   exact h0  
 have _ : tri_nd.1 IsACongrTo tri_nd.flip_vertices.1 := by
   apply acongr_of_SAS
-  simp only [← h21, h1]
-  rw [h23]
-  rw [← h22, ← h1]
-rw [IsACongr.angle₂.value, h24]
+  rw [← c, h1]
+  rw [x]
+  rw [← b, ← h1]
+rw [IsACongr.angle₂.value, z]
 intro k0
 have _ : tri_nd.1 IsACongrTo tri_nd.flip_vertices.1 := by
   apply acongr_of_ASA
-  rw [k0, h24]
+  rw [k0, z]
   exact a
   rw [← k0, y]
 have k2 := IsACongr.edge₂ tri_nd.1 tri_nd.flip_vertices.1
-rw [← h21] at k2
+rw [← c] at k2
 exact k2
-
 
 namespace Triangle
 
@@ -67,7 +53,6 @@ have h₂ : tri.flip_vertices.edge₂.length = tri.flip_vertices.edge₃.length 
 exact h₂
 
 end Triangle
-
 
 end IsocelesTriangles
 
@@ -105,8 +90,8 @@ end Triangle
 
 -- A nontrivial triangle is an regular triangle if and only if all of its angles are equal.
 theorem regular_tri_iff_eq_angle_of_nd_tri (tri_nd : Triangle_nd P) : tri_nd.1.IsRegular ↔ tri_nd.angle₁.value = tri_nd.angle₂.value ∧ tri_nd.angle₁.value = tri_nd.angle₃.value := by 
-rcases Triangle.angle_eq_angle_of_perm_vertices tri_nd with ⟨a1, a2, _⟩
-rcases Triangle.edge_eq_edge_of_perm_vertices tri_nd.1 with ⟨b1, b2, _⟩ 
+rcases Triangle_nd.angle_eq_angle_of_perm_vertices tri_nd with ⟨a1, a2, _⟩
+rcases Triangle_nd.edge_eq_edge_of_perm_vertices tri_nd with ⟨b1, b2, _⟩ 
 constructor
 intro h
 have perm_isisocele₁ : tri_nd.1.perm_vertices.IsIsoceles := by
@@ -152,7 +137,6 @@ rw [← h1, h3]
 rw [← h2, h3]
 norm_num
 
-
 -- An anticlockwise equilateral triangle has all angles being - π/3
 
 theorem ang_eq_sixty_deg_of_acclock_regular_tri (tri_nd : Triangle_nd P) (acclock : ¬ tri_nd.is_cclock)(h : tri_nd.1.IsRegular) : tri_nd.angle₁.value= - π / 3 ∧ tri_nd.angle₂.value = - π / 3 ∧ tri_nd.angle₃.value = - π / 3 := by 
@@ -173,37 +157,37 @@ norm_num
 
 theorem regular_tri_of_isoceles_tri_of_fst_ang_eq_sixty_deg(tri_nd : Triangle_nd P) (h : tri_nd.angle₁.value = π /3 ∨ tri_nd.angle₁.value = - π / 3)(h1 : tri_nd.1.IsIsoceles) : tri_nd.1.IsRegular := by 
 rcases h with (p1 | p2)
-have h3 : tri_nd.is_cclock := by  
+have cclock : tri_nd.is_cclock := by  
   apply Triangle.cclock_of_pos_angle
   left
   rw [p1]
   linarith [Real.pi_pos]
-have h5 := Triangle.angle_sum_eq_pi_of_cclock tri_nd h3
-have h7 : tri_nd.angle₂.value = π / 3 := by
-  rw [← is_isoceles_tri_iff_ang_eq_ang_of_nd_tri.mp h1] at h5
-  linarith [h5]
-have h8 : tri_nd.angle₃.value = π / 3 := by
-  simp only [← is_isoceles_tri_iff_ang_eq_ang_of_nd_tri.mp h1, h7]
+have sum₁ := Triangle.angle_sum_eq_pi_of_cclock tri_nd cclock
+have h2 : tri_nd.angle₂.value = π / 3 := by
+  rw [← is_isoceles_tri_iff_ang_eq_ang_of_nd_tri.mp h1] at sum₁
+  linarith [sum₁]
+have h3 : tri_nd.angle₃.value = π / 3 := by
+  simp only [← is_isoceles_tri_iff_ang_eq_ang_of_nd_tri.mp h1, h2]
 rw [regular_tri_iff_eq_angle_of_nd_tri]
 constructor
-rw [p1, h7]
-rw [p1, h8]
-have f3 : ¬ tri_nd.is_cclock := by
+rw [p1, h2]
+rw [p1, h3]
+have clock : ¬ tri_nd.is_cclock := by
   apply Triangle.clock_of_neg_angle
   left
   rw [p2]
   linarith [Real.pi_pos]
-have f5 := Triangle.angle_sum_eq_neg_pi_of_clock tri_nd  f3
-have f7 : tri_nd.angle₂.value = -π / 3 := by
-  rw [← is_isoceles_tri_iff_ang_eq_ang_of_nd_tri.mp h1, p2] at f5
-  linarith [f5]
-have f8 : tri_nd.angle₃.value = -π / 3 := by
+have sum₂ := Triangle.angle_sum_eq_neg_pi_of_clock tri_nd clock
+have f4 : tri_nd.angle₂.value = -π / 3 := by
+  rw [← is_isoceles_tri_iff_ang_eq_ang_of_nd_tri.mp h1, p2] at sum₂
+  linarith [sum₂]
+have f5 : tri_nd.angle₃.value = -π / 3 := by
   rw [← is_isoceles_tri_iff_ang_eq_ang_of_nd_tri.mp h1]
-  rw [f7]
+  rw [f4]
 rw [regular_tri_iff_eq_angle_of_nd_tri]
 constructor
-rw [p2 , f7]
-rw [p2 , f8]
+rw [p2 , f4]
+rw [p2 , f5]
 
 
 theorem regular_tri_of_isoceles_tri_of_snd_ang_eq_sixty_deg (tri_nd : Triangle_nd P) (h : tri_nd.angle₂.value = π /3 ∨ tri_nd.angle₂.value = - π / 3)(h1 : tri_nd.1.IsIsoceles) : tri_nd.1.IsRegular:= by 
