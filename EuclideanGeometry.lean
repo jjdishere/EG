2b71e4a112f842ec8416b13e8f62b635ec517c8c
jjdishere <emailboxofjjd@163.com>
2023-09-27 17:49:41 +0800

    add exercise to auto-build

--- EuclideanGeometry.lean
+++ EuclideanGeometry.lean
@@ -2,5 +2,7 @@ import EuclideanGeometry.Foundation.Index
 import EuclideanGeometry.Example.Index
 
 /-!
-#
+# Euclidean Geometry
+
+Welcome! This project aims to formalize the Euclidean Geometry in Lean. Please enjoy youeself in exploring with theorems and examples.
 -/
