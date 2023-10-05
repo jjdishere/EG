08f314c8d5c53235c039ffbd985ce92c29649f01
mbkybky <2065545849@qq.com>
2023-10-05 18:27:18 +0800

    Fills sorry's in Ray.lean

--- EuclideanGeometry/Foundation/Axiom/Linear/Ray.lean
+++ EuclideanGeometry/Foundation/Axiom/Linear/Ray.lean
@@ -179,10 +179,8 @@ theorem Seg.lies_on_of_lies_int {p : P} (h : p LiesInt seg) : p LiesOn seg := h.
 
 theorem Seg.lies_int_iff (p : P) : p LiesInt seg ↔ seg.is_nd ∧ ∃ (t : ℝ) , 0 < t ∧ t < 1 ∧ VEC seg.1 p = t • seg.toVec := by
   constructor
-  · intro ⟨lieson, ns, nt⟩
+  · intro ⟨⟨t, tnonneg, tle1, ht⟩, ns, nt⟩
     rw [ne_iff_vec_ne_zero] at ns nt
-    rw [Seg.IsOn] at lieson
-    rcases lieson with ⟨t, tnonneg, tle1, ht⟩
     constructor
     · rw [Seg.is_nd]
       contrapose! ns
@@ -211,7 +209,7 @@ theorem Seg.lies_int_iff (p : P) : p LiesInt seg ↔ seg.is_nd ∧ ∃ (t : ℝ)
         rw [ne_iff_vec_ne_zero, ← vec_sub_vec seg.source, ht, toVec, this, smul_ne_zero_iff]
         constructor
         · linarith
-        · simp only [Seg.toVec,← ne_iff_vec_ne_zero]
+        · simp only [Seg.toVec, ← ne_iff_vec_ne_zero]
           exact nd
 
 theorem Ray.lies_on_of_lies_int {p : P} (h : p LiesInt ray) : p LiesOn ray := h.1
@@ -265,6 +263,8 @@ theorem seg_toVec_eq_vec (A B : P) : (SEG A B).toVec = VEC A B := rfl
 theorem toVec_eq_zero_of_deg {l : Seg P} : (l.target = l.source) ↔ l.toVec = 0 := by
   rw [Seg.toVec, Vec.mk_pt_pt, vsub_eq_zero_iff_eq]
 
+theorem Seg.is_nd_iff_toVec_ne_zero {l : Seg P} : l.is_nd ↔ l.toVec ≠ 0 := toVec_eq_zero_of_deg.not
+
 section length
 
 variable {l : Seg P}
@@ -276,124 +276,152 @@ def Seg.length (l : Seg P) : ℝ := norm (l.toVec)
 theorem length_nonneg : 0 ≤ l.length := norm_nonneg _
 
 -- A generalized directed segment is nontrivial if and only if its length is positive.
-theorem length_pos_iff_nd : 0 < l.length ↔ (l.is_nd) := by
-  rw [Seg.length, Seg.is_nd, norm_pos_iff]
-  exact (toVec_eq_zero_of_deg).symm.not
+theorem length_pos_iff_nd : 0 < l.length ↔ l.is_nd :=
+  norm_pos_iff.trans toVec_eq_zero_of_deg.symm.not
 
-theorem length_ne_zero_iff_nd : 0 ≠ l.length ↔ (l.is_nd) := by
-  apply Iff.not
-  rw [toVec_eq_zero_of_deg, eq_comm]
-  exact norm_eq_zero
+theorem length_ne_zero_iff_nd : 0 ≠ l.length ↔ l.is_nd := 
+  (ne_iff_lt_iff_le.mpr (norm_nonneg _)).trans length_pos_iff_nd
 
+theorem length_pos (l : Seg_nd P) : 0 < l.1.length := length_pos_iff_nd.mpr l.2
 
-theorem length_pos (l : Seg_nd P): 0 < l.1.length := by
-  rw [length_pos_iff_nd]
-  simp only [l.2, not_false_eq_true]
-
-
-theorem length_sq_eq_inner_toVec_toVec : l.length ^ 2 = inner l.toVec l.toVec := by
-  rw [Seg.length]
-  exact Eq.symm (real_inner_self_eq_norm_sq (Seg.toVec l))
+theorem length_sq_eq_inner_toVec_toVec : l.length ^ 2 = inner l.toVec l.toVec :=
+  (real_inner_self_eq_norm_sq (Seg.toVec l)).symm
 
 -- A generalized directed segment is trivial if and only if length is zero.
-theorem triv_iff_length_eq_zero : (l.target = l.source) ↔ l.length = 0 := by
-  exact Iff.trans (toVec_eq_zero_of_deg)  (@norm_eq_zero _ _).symm
+theorem triv_iff_length_eq_zero : (l.target = l.source) ↔ l.length = 0 :=
+  (toVec_eq_zero_of_deg).trans norm_eq_zero.symm
 
 -- If P lies on a generalized directed segment AB, then length(AB) = length(AP) + length(PB)
 theorem length_eq_length_add_length (l : Seg P) (A : P) (lieson : A LiesOn l) : l.length = (SEG l.source A).length + (SEG A l.target).length := by
-  unfold Seg.length
-  repeat rw [seg_toVec_eq_vec]
-  rcases lieson with ⟨t, ⟨a, b, c⟩ ⟩
-  have h: VEC l.source l.target = VEC l.source A + VEC A l.target := by rw [vec_add_vec]
-  rw [c]
-  have s: VEC A l.target = ( 1 - t ) • VEC l.source l.target := by 
+  rcases lieson with ⟨t, ⟨a, b, c⟩⟩
+  have h : VEC l.source l.target = VEC l.source A + VEC A l.target := by rw [vec_add_vec]
+  have s : VEC A l.target = (1 - t) • VEC l.source l.target := by 
     rw [c] at h
     rw [sub_smul, one_smul]
-    exact eq_sub_of_add_eq' (id (Eq.symm h))
-  rw [s, norm_smul, norm_smul, ← add_mul, Real.norm_of_nonneg, Real.norm_of_nonneg]
+    exact eq_sub_of_add_eq' h.symm
+  rw [Seg.length, Seg.length, Seg.length, seg_toVec_eq_vec, seg_toVec_eq_vec, seg_toVec_eq_vec, c, s,
+    norm_smul, norm_smul, ← add_mul, Real.norm_of_nonneg a, Real.norm_of_nonneg (sub_nonneg.mpr b)]
   linarith
-  rw [sub_nonneg]
-  exact b
-  exact a
 
 end length
 
 section midpoint
 
-variable {seg : Seg P} {seg_nd : Seg_nd P}
+variable (seg : Seg P) (seg_nd : Seg_nd P) {A : P} {l : Seg P} 
 
-def Seg.midpoint (seg : Seg P) : P := (1 / 2 : ℝ) • (seg.toVec) +ᵥ seg.source
+def Seg.midpoint : P := (1 / 2 : ℝ) • seg.toVec +ᵥ seg.source
 
-theorem Seg.midpt_lies_on : seg.midpoint LiesOn seg := sorry
+theorem Seg.vec_source_midpt : VEC seg.1 seg.midpoint = 1 / 2 * VEC seg.1 seg.2 := by
+  simp only [midpoint, one_div, Complex.real_smul, Complex.ofReal_inv, vec_of_pt_vadd_pt_eq_vec]
+  rfl
 
-theorem Seg_nd.midpt_lies_int (seg_nd : Seg_nd P) : seg_nd.1.midpoint LiesInt seg_nd.1 := sorry
+theorem Seg.vec_midpt_target : VEC seg.midpoint seg.2 = 1 / 2 * VEC seg.1 seg.2 := by
+  rw [midpoint, ← vec_add_vec _ seg.1 _, ← neg_vec, vec_of_pt_vadd_pt_eq_vec]
+  field_simp
+  calc
+    _ = VEC seg.1 seg.2 * (- 1) + VEC seg.1 seg.2 * 2 := by 
+      rw [mul_neg, mul_one]
+      rfl
+    _ = _ := by 
+      rw [← mul_add]
+      norm_num
 
--- A point is the mid opint of a segment if and only it defines the same vector to the source and the target of the segment
-theorem midpt_iff_same_vector_to_source_and_target {A : P} {l : Seg P} : A = l.midpoint ↔ (SEG l.source A).toVec = (SEG A l.target).toVec := by sorry
+theorem Seg.vec_midpt_eq : VEC seg.1 seg.midpoint = VEC seg.midpoint seg.2 := by 
+  rw[seg.vec_source_midpt, seg.vec_midpt_target]
 
-theorem dist_target_eq_dist_source_of_midpt : (SEG seg.source seg.midpoint).length = (SEG seg.midpoint seg.target).length := sorry
+theorem Seg.vec_eq_of_eq_midpt (h : A = l.midpoint) : VEC l.1 A = VEC A l.2 := by 
+  rw [h]
+  exact l.vec_midpt_eq
 
-theorem eq_midpoint_iff_in_seg_and_dist_target_eq_dist_source {A : P} : A = seg.midpoint ↔ (A LiesOn seg) ∧ (SEG seg.source A).length = (SEG A seg.target).length := sorry
+theorem midpt_of_vector_from_source (h : VEC l.1 A = 1 / 2 * VEC l.1 l.2) : 
+    A = l.midpoint := by
+  rw [← start_vadd_vec_eq_end l.1 A, h, Seg.midpoint, Complex.real_smul]
+  norm_num
+  rfl
+
+theorem midpt_of_vector_to_target (h : VEC A l.2 = 1 / 2 * VEC l.1 l.2) : 
+    A = l.midpoint := by
+  refine' midpt_of_vector_from_source _
+  nth_rw 1 [eq_sub_of_add_eq (vec_add_vec l.1 A l.2), h, ← one_mul (VEC l.1 l.2), ← sub_mul]
+  norm_num
+
+theorem midpt_of_same_vector_to_source_and_target (h : VEC l.1 A = VEC A l.2) : 
+    A = l.midpoint := by
+  refine' midpt_of_vector_from_source _
+  field_simp
+  rw [mul_two, ← vec_add_vec l.1 A l.2]
+  exact congrArg (HAdd.hAdd _) h
+
+theorem Seg.midpt_lies_on : seg.midpoint LiesOn seg := ⟨1 / 2, by norm_num; exact seg.vec_source_midpt⟩
+
+theorem Seg.lies_on_of_eq_midpt (h : A = l.midpoint) : A LiesOn l := by
+  rw [h]
+  exact l.midpt_lies_on
+
+theorem Seg_nd.midpt_lies_int : seg_nd.1.midpoint LiesInt seg_nd.1 :=
+  (Seg.lies_int_iff seg_nd.1.midpoint).mpr ⟨seg_nd.2, ⟨1 / 2, by norm_num; exact seg_nd.1.vec_source_midpt⟩⟩
+
+/-- A point $X$ on a given segment $AB$ is the midpoint if and only if the vector $\overrightarrow{AX}$ is the same as the vector $\overrightarrow{XB}$. -/
+theorem midpt_iff_same_vector_to_source_and_target (A : P) (l : Seg P) : A = l.midpoint ↔ (SEG l.source A).toVec = (SEG A l.target).toVec :=
+  ⟨fun h ↦ Seg.vec_eq_of_eq_midpt h, fun h ↦ midpt_of_same_vector_to_source_and_target h⟩
+
+theorem dist_target_eq_dist_source_of_midpt : (SEG seg.source seg.midpoint).length = (SEG seg.midpoint seg.target).length := congrArg norm seg.vec_midpt_eq
+
+theorem dist_target_eq_dist_source_of_eq_midpt (h : A = l.midpoint) : (SEG l.1 A).length = (SEG A l.2).length := by
+  rw [h]
+  exact dist_target_eq_dist_source_of_midpt l
+
+theorem eq_midpoint_iff_in_seg_and_dist_target_eq_dist_source (A : P) (l : Seg P) : A = l.midpoint ↔ (A LiesOn l) ∧ (SEG l.source A).length = (SEG A l.target).length := by
+  refine' ⟨fun h ↦ ⟨Seg.lies_on_of_eq_midpt h, dist_target_eq_dist_source_of_eq_midpt h⟩, _⟩
+  intro ⟨⟨t, ht0, ht1, ht⟩, hv⟩
+  have hv : ‖VEC l.1 A‖ = ‖VEC A l.2‖ := hv
+  by_cases h0 : ‖VEC A l.2‖ = 0
+  · apply midpt_of_same_vector_to_source_and_target
+    rw [h0] at hv
+    rw [norm_eq_zero.mp h0, norm_eq_zero.mp hv]
+  · have h := ht
+    rw [← one_smul ℝ (VEC l.1 A), ← vec_add_vec l.1 A l.2, smul_add, add_comm] at h
+    have h := sub_eq_of_eq_add h
+    rw [← sub_smul 1 t _] at h
+    have h := congrArg norm h
+    simp only [norm_smul, hv, Real.norm_of_nonneg ht0, Real.norm_of_nonneg (sub_nonneg.mpr ht1)] at h
+    have h : t = 1 / 2 := by
+      apply eq_one_div_of_mul_eq_one_left
+      rw [mul_two]
+      exact (eq_add_of_sub_eq (mul_right_cancel₀ h0 h)).symm
+    exact midpt_of_vector_from_source (by rw [ht, h]; norm_num)
 
 end midpoint
 
 section existence
 
-variable {l : Seg P}
-
 -- Archimedean property I : given a directed segment AB (with A ≠ B), then there exists a point P such that B lies on the directed segment AP and P ≠ B.
 
-theorem Seg_nd.exist_pt_beyond_pt {P : Type _} [EuclideanPlane P] (l : Seg_nd P) : (∃ q : P, l.1.target LiesInt (SEG l.1.source q)) := by 
-  let h := l.1.toVec +ᵥ l.1.target
-  let half : ℝ := 1/2
-  have c: 0 ≤ half ∧ half ≤ 1 ∧ VEC l.1.source l.1.target = half • VEC l.1.source h := by
-    norm_num
-    rw [seg_toVec_eq_vec, Vec.mk_pt_pt, Vec.mk_pt_pt]
-    field_simp
-    rw [vadd_vsub_assoc]
-    exact mul_two (l.1.target -ᵥ l.1.source)
-  have b: l.1.target ≠ l.1.source ∧ l.1.target ≠ h := by
-    constructor
-    exact l.2
-    have x: l.1.toVec ≠ 0 := by 
-      rw [seg_toVec_eq_vec, Vec.mk_pt_pt, vsub_ne_zero]
-      exact l.2
-    have y: l.1.target ≠ l.1.toVec +ᵥ l.1.target := by
-      rw [ne_comm]
-      by_contra t
-      rw [vadd_eq_self_iff_vec_eq_zero] at t 
-      exact x t
-    exact y
-  have k: l.1.target LiesInt SEG l.1.source h := ⟨ ⟨half, c⟩, b⟩
-  use h
+theorem Seg_nd.exist_pt_beyond_pt (l : Seg_nd P) : (∃ q : P, l.1.target LiesInt (SEG l.1.source q)) := by
+  refine' ⟨l.1.toVec +ᵥ l.1.target, ⟨1 / 2, _⟩, 
+    ⟨l.2, fun t ↦ (Seg.is_nd_iff_toVec_ne_zero.mp l.2) (vadd_eq_self_iff_vec_eq_zero.mp t.symm)⟩⟩
+  norm_num
+  rw [seg_toVec_eq_vec, Vec.mk_pt_pt, Vec.mk_pt_pt]
+  field_simp
+  rw [vadd_vsub_assoc]
+  exact mul_two (l.1.target -ᵥ l.1.source)
  
 -- Archimedean property II: On an nontrivial directed segment, one can always find a point in its interior.  `This will be moved to later disccusion about midpoint of a segment, as the midpoint is a point in the interior of a nontrivial segment`
 
-theorem nd_of_exist_int_pt (l : Seg P) (p : P) (h : p LiesInt l) : l.is_nd := by
-  rw [Seg.is_nd]
-  rcases h with ⟨⟨c, d⟩, b⟩
-  rcases b with ⟨p_ne_s, _⟩
-  rcases d with ⟨_, _, e⟩
-  have t: VEC Seg.source p ≠ 0 := by exact Iff.mp (ne_iff_vec_ne_zero Seg.source p) p_ne_s
+theorem nd_of_exist_int_pt {p : P} {l : Seg P} (h : p LiesInt l) : l.is_nd := by
+  rcases h with ⟨⟨_, ⟨_, _, e⟩⟩, ⟨p_ne_s, _⟩⟩
+  have t : VEC Seg.source p ≠ 0 := (ne_iff_vec_ne_zero Seg.source p).mp p_ne_s
   rw [e] at t
   exact Iff.mp vsub_ne_zero (right_ne_zero_of_smul t)
 
 -- If a generalized directed segment contains an interior point, then it is nontrivial
-theorem nd_iff_exist_int_pt (l : Seg P) : (∃ (p : P), p LiesInt l) ↔ l.is_nd := by
-  constructor
-  intro h
-  rcases h with ⟨a, b⟩
-  exact nd_of_exist_int_pt l a b
-  intro h
-  use l.midpoint
-  exact Seg_nd.midpt_lies_int ⟨l, h⟩
+theorem nd_iff_exist_int_pt (l : Seg P) : (∃ (p : P), p LiesInt l) ↔ l.is_nd :=
+  ⟨fun ⟨_, b⟩ ↦ nd_of_exist_int_pt b, fun h ↦ ⟨l.midpoint, Seg_nd.midpt_lies_int ⟨l, h⟩⟩⟩
 
-theorem Seg_nd.exist_int_pt (l : Seg_nd P) : ∃ (p : P), p LiesInt l.1 := by
-  use l.1.midpoint
-  exact midpt_lies_int l
+theorem Seg_nd.exist_int_pt (l : Seg_nd P) : ∃ (p : P), p LiesInt l.1 := ⟨l.1.midpoint, midpt_lies_int l⟩
 
-theorem length_pos_iff_exist_int_pt (l : Seg P) : 0 < l.length ↔ (∃ (p : P), p LiesInt l) := by 
-  exact Iff.trans (length_pos_iff_nd) (nd_iff_exist_int_pt l).symm
+theorem length_pos_iff_exist_int_pt (l : Seg P) : 0 < l.length ↔ (∃ (p : P), p LiesInt l) :=
+  length_pos_iff_nd.trans (nd_iff_exist_int_pt l).symm
 
 end existence
 
