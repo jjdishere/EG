5213246fecd19ba2c54e6f3ff4c47fa1f425b193
mathzhuonichi <89577211+mathzhuonichi@users.noreply.github.com>
2023-10-25 03:29:02 +0000

    Almost_finished_1.7_formalized_1.8

--- EuclideanGeometry/Example/ShanZun/Ex1c.lean
+++ EuclideanGeometry/Example/ShanZun/Ex1c.lean
@@ -9,9 +9,10 @@ namespace EuclidGeom
 variable {P : Type _} [EuclideanPlane P]
 
 namespace Shan_Problem_1_7
-/- In $\triangle ABC$, $\angle ACB = \pi /2$. Let $D$ be the midpoint of $AB$. 
+/- In $\triangle ABC$, $\angle ACB = \pi /2$. Let $D$ be the midpoint of $AB$.
 
 Prove that $CD = AB / 2$. -/
+
 variable {A B C: P} {hnd : ¬ colinear A B C}
 -- Claim: $A \ne B$ and $A \ne C$ and $B \ne C$.
 -- This is because vertices of nondegenerate triangles are distinct.
@@ -19,15 +20,15 @@ lemma b_ne_a : B ≠ A := (ne_of_not_colinear hnd).2.2
 lemma c_ne_a : C ≠ A := (ne_of_not_colinear hnd).2.1.symm
 lemma b_ne_c : B ≠ C := (ne_of_not_colinear hnd).1.symm
 --∠ A C B = π/2
-variable {hrt : (ANG A C B c_ne_a.symm b_ne_c).IsRightAngle} 
--- D is the midpoint of segment AB 
+variable {hrt : (ANG A C B c_ne_a.symm b_ne_c).IsRightAngle}
+-- D is the midpoint of segment AB
 variable {D : P} {hd : D = (SEG A B).midpoint}
 lemma d_ne_a: D ≠ A := by
   rw[hd]
   apply (Seg_nd.midpt_lies_int (SEG_nd A B (b_ne_a).symm)).2.1
   use C
   by_contra h
-  have : colinear A B C :=by 
+  have : colinear A B C :=by
     apply flip_colinear_fst_snd h
   trivial
 --Introduce the midpoint E of AC
@@ -53,20 +54,24 @@ lemma aec_colinear : colinear A E C := by
   use 2
   simp only [he,Seg.midpoint,one_div, seg_toVec_eq_vec, vec_of_pt_vadd_pt_eq_vec,smul_smul]
   norm_num
+
 lemma midpt_half_length : (SEG A D).length = (SEG A B).length/2:=by
   rw[length_eq_length_add_length (SEG A B) D,← dist_target_eq_dist_source_of_eq_midpt,half_add_self]
   simp only [Seg.source]
   exact hd
   rw[hd]
   exact Seg.midpt_lies_on (SEG A B)
+
 lemma ad_ratio : (SEG A D).length / (SEG A B).length = 2⁻¹ :=by
   apply div_eq_of_eq_mul
   apply (length_ne_zero_iff_nd.mpr (b_ne_a)).symm
   use C
   exact hnd
-  rw[mul_comm,← (div_eq_mul_inv ((SEG A B).length) 2)]
-  apply midpt_half_length
+  rw[length_eq_length_add_length (SEG A B) D,← dist_target_eq_dist_source_of_eq_midpt]
+  simp only [Seg.source,←mul_two,mul_comm,←mul_assoc,ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, mul_inv_cancel, one_mul]
   exact hd
+  rw[hd]
+  apply Seg.midpt_lies_on (SEG A B)
 lemma ae_ratio : (SEG A E).length / (SEG A C).length = 2⁻¹ :=by
   apply div_eq_of_eq_mul
   apply (length_ne_zero_iff_nd.mpr (c_ne_a)).symm
@@ -98,9 +103,9 @@ lemma hnd': ¬ colinear A D E := by
 lemma hnd'' : ¬ colinear C D E := by
   intro h
   have : colinear C D A := by
-    apply flip_colinear_snd_trd 
+    apply flip_colinear_snd_trd
     apply colinear_of_colinear_colinear_ne
-    apply (flip_colinear_snd_trd (flip_colinear_fst_snd (flip_colinear_snd_trd aec_colinear))) 
+    apply (flip_colinear_snd_trd (flip_colinear_fst_snd (flip_colinear_snd_trd aec_colinear)))
     use E
     exact he
     apply flip_colinear_snd_trd h
@@ -111,7 +116,7 @@ lemma hnd'' : ¬ colinear C D E := by
     apply colinear_of_colinear_colinear_ne
     apply adb_colinear
     apply hd
-    apply (flip_colinear_snd_trd (flip_colinear_fst_snd (flip_colinear_snd_trd this))) 
+    apply (flip_colinear_snd_trd (flip_colinear_fst_snd (flip_colinear_snd_trd this)))
     apply d_ne_a
     apply hnd
     exact hd
@@ -134,7 +139,7 @@ lemma ade_sim_abc: TRI_nd A D E (@hnd' P _ A B C hnd D hd E he)∼ TRI_nd A B C 
   use B
   exact hnd
   exact he
---rays equal respectively
+--rays equal, respectively
   apply Angle.ext
   have h₀:(TRI_nd A D E (@hnd' P _ A B C hnd D hd E he)).angle₁.1=(SEG_nd A D (@d_ne_a P _ A B C hnd D hd)).toRay := rfl
   rw[h₀]
@@ -163,13 +168,10 @@ lemma ade_sim_abc: TRI_nd A D E (@hnd' P _ A B C hnd D hd E he)∼ TRI_nd A B C 
   simp only [Seg.toVec,he]
   apply Seg.vec_source_midpt
 
---need to define right angle
-lemma ade_acongr_cde : (TRI_nd A D E (@hnd' P _ A B C hnd D hd E he)).1 IsACongrTo (TRI_nd C D E (@hnd'' P _ A B C hnd D hd E he)).1:= by
-  apply acongr_of_SAS
-  simp only [Triangle.edge₂ , Triangle.point₃]
-  sorry
-  sorry
-  sorry
+lemma ade_acongr_cde : (TRI_nd A D E (@hnd' P _ A B C hnd D hd E he)).1 IsACongrTo (TRI_nd C D E (@hnd'' P _ A B C hnd D hd E he)).1 := sorry
+  --need to define the value of right angle
+  --by_SAS (acongr)
+
 lemma cd : (TRI C D E).edge₃.length = (SEG C D).length:=by
   simp only [Triangle.edge₃]
 lemma ad : (TRI A D E).edge₃.length = (SEG A D).length:=by
@@ -189,7 +191,7 @@ lemma ad_eq_cd: (SEG A D).length = (SEG C D).length  := by
     unfold Seg.source Triangle.edge₃
     simp only
     exact he
-    
+
 theorem Shan_Problem_1_7 : (SEG C D).length = (SEG A B).length/2 := by
   rw[←ad_eq_cd]
   apply midpt_half_length
@@ -202,10 +204,19 @@ theorem Shan_Problem_1_7 : (SEG C D).length = (SEG A B).length/2 := by
 end Shan_Problem_1_7
 
 namespace Shan_Problem_1_8
-/- In $\triangle ABC$, let $BD$ and $CE$ be the heights, with foots $D$ and $E$, respectively. Let $F$ and $G$ be the midpoint of $BC$ and $DE$, respectively. 
+/- In $\triangle ABC$, let $BD$ and $CE$ be the heights, with foots $D$ and $E$, respectively. Let $F$ and $G$ be the midpoint of $BC$ and $DE$, respectively.
 
 Prove that $FG \perp DE$. -/
-
-
-
+variable {A B C : P} {hnd : ¬ colinear A B C}
+lemma b_ne_a : B ≠ A := (ne_of_not_colinear hnd).2.2
+lemma c_ne_a : C ≠ A := (ne_of_not_colinear hnd).2.1.symm
+lemma b_ne_c : B ≠ C := (ne_of_not_colinear hnd).1.symm
+--introduce the perps
+variable {D : P} {hd : D = perp_foot B (SEG_nd A C c_ne_a).toLine}
+variable {E : P} {he : E = perp_foot C (SEG_nd A B b_ne_a).toLine}
+variable {F G: P} {hf : F = (SEG B C).midpoint} {hg : G = (SEG D E).midpoint}
+lemma e_ne_d: E ≠ D := sorry
+lemma g_ne_f: G ≠ F := sorry
+--Failed to use the notation ⟂
+theorem Shan_Problem_1_8:(SEG_nd F G g_ne_f).toLine.toProj = (SEG_nd D E e_ne_d).toLine.toProj.perp := sorry
 end Shan_Problem_1_8
