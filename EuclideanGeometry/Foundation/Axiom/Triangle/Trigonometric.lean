dc5e9551860d68944040a24d1995eb6e742ccfa7
Autrilance <145187991+Autrilance@users.noreply.github.com>
2023-10-31 21:00:37 +0800

    Some adjustments to the comments.

--- EuclideanGeometry/Foundation/Axiom/Triangle/Trigonometric.lean
+++ EuclideanGeometry/Foundation/Axiom/Triangle/Trigonometric.lean
@@ -78,6 +78,7 @@ theorem Pythagoras_of_perp_foot (A B : P) {l : Line P} (h : B LiesOn l) : (SEG A
   --let B := tr_nd.1.point₂
   --let C := tr_nd.1.point₃
 
+/-- Given ▵ A B C with ∠ B A C = π / 2, A B ^ 2 + A C ^ 2 = B C ^ 2. -/
 theorem Pythagoras_of_right_triangle_non_trivial (A B C : P) {hnd : ¬ colinear A B C} (right_triangle: ∠ B A C (ne_of_not_colinear hnd).2.2 (ne_of_not_colinear hnd).2.1.symm = π / 2 ) : (SEG A B).length ^ 2 + (SEG A C).length ^ 2 = (SEG B C).length ^ 2 := by
   have h : Real.cos (∠ B A C (ne_of_not_colinear hnd).2.2 (ne_of_not_colinear hnd).2.1.symm) = 0 := by
     rw [right_triangle]
