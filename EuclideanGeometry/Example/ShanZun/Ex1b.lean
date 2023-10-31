abd899386cde9df05181aaf4988bba0a4cfe1e7a
kfc2333 <48116356+kfc2333@users.noreply.github.com>
2023-10-31 20:23:36 +0800

    deleted LinearObj.seg_nd

--- EuclideanGeometry/Example/ShanZun/Ex1b.lean
+++ EuclideanGeometry/Example/ShanZun/Ex1b.lean
@@ -9,7 +9,7 @@ namespace EuclidGeom
 variable {P : Type _} [EuclideanPlane P]
 
 namespace Shan_Problem_1_5
-/- In $\triangle ABC$, let $AD$ be the median.  Let $E$ be a point on $AD$ such that $BE = AC$. The line $BE$ intersects $AC$ at $F$. 
+/- In $\triangle ABC$, let $AD$ be the median.  Let $E$ be a point on $AD$ such that $BE = AC$. The line $BE$ intersects $AC$ at $F$.
 
 Prove that $AF = EF$. -/
 
@@ -20,8 +20,8 @@ def tri : Triangle P := ▵ A B C
 lemma a_ne_c : A ≠ C := (ne_of_not_colinear hnd).2.1
 lemma b_ne_c : B ≠ C := (ne_of_not_colinear hnd).1.symm
 -- Claim: $BC$ is non degenerate. This is because $B \ne C$.
-def seg_nd_ac : Seg_nd P := ⟨(SEG A C), Ne.symm (a_ne_c (hnd := hnd) ) ⟩ 
-def seg_nd_bc : Seg_nd P := ⟨(SEG B C), Ne.symm (b_ne_c (hnd := hnd) ) ⟩ 
+def seg_nd_ac : Seg_nd P := ⟨(SEG A C), Ne.symm (a_ne_c (hnd := hnd) ) ⟩
+def seg_nd_bc : Seg_nd P := ⟨(SEG B C), Ne.symm (b_ne_c (hnd := hnd) ) ⟩
 -- Let $D$ be the midpoint of $BC$.
 variable {D : P} {d_mid : D = (SEG B C).midpoint}
 -- $D$ Liesin $BC$. This is because $BC$ is non degenerate.
@@ -37,14 +37,14 @@ lemma b_ne_e : B ≠ E := by
   rw [ne_eq]
   rw [(triv_iff_length_eq_zero (l := SEG B E)).symm.not] at h
   exact Ne.symm h
-def seg_nd_be : Seg_nd P := ⟨SEG B E, Ne.symm (b_ne_e (hnd := hnd) (be_eq_ac := be_eq_ac))⟩ 
-variable {be_not_parallel_ac : ¬ LinearObj.seg_nd (seg_nd_be (hnd := hnd) (be_eq_ac := be_eq_ac)) ∥ (seg_nd_ac (hnd := hnd))}
+def seg_nd_be : Seg_nd P := ⟨SEG B E, Ne.symm (b_ne_e (hnd := hnd) (be_eq_ac := be_eq_ac))⟩
+variable {be_not_parallel_ac : ¬ (seg_nd_be (hnd := hnd) (be_eq_ac := be_eq_ac)) ∥ (seg_nd_ac (hnd := hnd))}
 theorem Shan_Problem_1_5 : (SEG A F).length = (SEG E F).length := by sorry
 
 end Shan_Problem_1_5
 
 namespace Shan_Problem_1_6
-/- Let $\triangle ABC$ be an isosceles triangle in which $AB = AC$. 
+/- Let $\triangle ABC$ be an isosceles triangle in which $AB = AC$.
 
 Prove that For any point $D$ on the base $BC$, the sum of the the distance of $D$ to $AB$ and to $AC$ is independent of $D$. -/
 
