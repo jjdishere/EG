7c60c03e04ac3be0efccf7a8e569da59da0697f2
cybcatppuccino <1538580589@qq.com>
2023-11-20 00:39:59 +0800

    Update Parallel_trash.lean
    no more seg_nd

--- EuclideanGeometry/Foundation/Axiom/Linear/Parallel_trash.lean
+++ EuclideanGeometry/Foundation/Axiom/Linear/Parallel_trash.lean
@@ -6,7 +6,7 @@ namespace EuclidGeom
 variable {P : Type _} [EuclideanPlane P]
 
 -- Later we need a version of this theorem in class DirFig
-theorem Seg_nd.para_self_rev (seg_nd : Seg_nd P) : seg_nd ∥ seg_nd.reverse := Eq.symm (Seg_nd.toproj_of_rev_eq_toproj seg_nd)
+theorem Seg_nd.para_self_rev (seg_nd : Seg_nd P) : seg_nd ∥ seg_nd.reverse := Eq.symm (toproj_of_rev_eq_toproj)
 -- The followings are corollaries:
 
 theorem Seg_nd.para_rev_of_para (seg_nd seg_nd' : Seg_nd P) : (seg_nd ∥ seg_nd') ↔ (seg_nd ∥ seg_nd'.reverse) := ⟨fun z => Eq.trans z (Seg_nd.para_self_rev seg_nd'), fun z => Eq.trans z (Eq.symm (Seg_nd.para_self_rev seg_nd'))⟩
