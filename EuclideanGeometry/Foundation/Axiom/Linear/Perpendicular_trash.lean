6619e00c78f516fc8550a48cd63e1ae3bffdb8d5
Mosente <150507609+Mosente@users.noreply.github.com>
2023-11-19 10:42:39 +0800

    1

---
+++ EuclideanGeometry/Foundation/Axiom/Linear/Perpendicular_trash.lean
@@ -0,0 +1,17 @@
+import EuclideanGeometry.Foundation.Axiom.Linear.Parallel
+import EuclideanGeometry.Foundation.Axiom.Linear.Perpendicular
+
+noncomputable section
+namespace EuclidGeom
+
+open Line
+
+variable {P : Type _} [EuclideanPlane P]
+
+section Perpendicular_constructions
+
+theorem pt_to_perp_foot_perp_line {A : P} {l : Line P} (h : ¬ (A LiesOn l)) : (LIN A (perp_foot A l) ((not_congr (perp_foot_eq_self_iff_lies_on A l)).mpr h)) ⟂ l := by sorry
+
+end Perpendicular_constructions
+
+end EuclidGeom
