52a00d64a2dd4b8f0ae34c6dc2d31a2f246286d4
Thmoas-Guan <150537269+Thmoas-Guan@users.noreply.github.com>
2023-11-19 10:40:23 +0800

    Schaum

---
+++ EuclideanGeometry/Example/SCHAUM/Problem1.2.lean
@@ -0,0 +1,35 @@
+import EuclideanGeometry.Foundation.Index
+
+noncomputable section
+
+namespace EuclidGeom
+
+variable {Plane : Type _} [EuclideanPlane Plane]
+
+namespace Problem1_2_
+/-Let $\triangle ABC$ be an isosceles triangle in which $AB = AC$.Let $D$ be a point on $AB$.
+Let $E$ be a point on $AC$ such that $AE = AD$.Let $P$ be the foot of perpendicular from $D$ to $BC$.
+Let $Q$ be the foot of perpendicular from $E$ to $BC$.
+
+Prove that $DP = EQ$.
+-/
+
+--Let $\triangle ABC$ be an isosceles triangle in which $AB = AC$.
+variable {A B C : Plane} {hnd: ¬ colinear A B C} {hisoc: (▵ A B C).IsIsoceles}
+--Let $D$ be a point on $AB$.
+variable {D : Plane} {D_on_seg: D LiesInt (SEG A B)}
+--Let $E$ be a point on $AC$
+variable {E : Plane} {E_on_seg: E LiesInt (SEG A B)}
+--such that $AE = AD$.
+variable {E_ray_position : (SEG A E).length = (SEG A D).length}
+-- Claim:$B \ne C$.
+-- This is because vertices  B C of nondegenerate triangles are distinct.
+lemma b_ne_c : B ≠ C := (ne_of_not_colinear hnd).1.symm
+-- Let $P$ be the foot of perpendicular from $D$ to $BC$.
+variable {P : Plane} {hd : P = perp_foot D (LIN B C b_ne_c.symm)}
+-- Let $Q$ be the foot of perpendicular from $E$ to $BC$.
+variable {Q : Plane} {hd : Q = perp_foot E (LIN B C b_ne_c.symm)}
+-- Prove that $DP = EQ$.
+theorem Problem1_2_ : (SEG D M).length = (SEG E M).length := by sorry
+
+end Problem1_2_
