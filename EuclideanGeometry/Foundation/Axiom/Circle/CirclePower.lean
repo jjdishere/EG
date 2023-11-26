01f461df8cde4df7ded92fcdb025f08ab5508c26
Noaillesss <nekodesu10086@gmail.com>
2023-11-26 12:47:36 +0800

    11.26
    Circle/Basic: add two methods to make a circle; move the contents of power to CirclePower.lean
    Circle/LCPosition: fill sorrys
    Circle/CirclePower: write the basic constructions
    Linear/Ray_trash: add three theorems

---
+++ EuclideanGeometry/Foundation/Axiom/Circle/CirclePower.lean
@@ -0,0 +1,80 @@
+import EuclideanGeometry.Foundation.Axiom.Circle.Basic
+import EuclideanGeometry.Foundation.Axiom.Circle.CCPosition
+import EuclideanGeometry.Foundation.Axiom.Circle.LCPosition
+
+noncomputable section
+namespace EuclidGeom
+
+variable {P : Type _} [EuclideanPlane P]
+
+namespace Circle
+
+-- Define the power of a point P relative to a circle ω with center O and radius r to be OP ^ 2 - r ^ 2
+def power (ω : Circle P) (p : P) : ℝ := dist ω.center p ^ 2 - ω.radius ^ 2
+
+theorem inside_circle_iff_power_npos (p : P) (ω : Circle P) : p LiesIn ω ↔ ω.power p ≤  0 := sorry
+
+theorem interior_of_circle_iff_power_neg (p : P) (ω : Circle P) : p LiesInt ω ↔ ω.power p < 0 := sorry
+
+theorem lies_on_circle_iff_power_zero (p : P) (ω : Circle P) : p LiesOn ω ↔ ω.power p = 0 := sorry
+
+theorem outside_circle_iff_power_pos (p : P) (ω : Circle P) : p LiesOut ω ↔ 0 < ω.power p  := sorry
+
+end Circle
+
+section tangent
+
+namespace Circle
+
+@[ext]
+structure Tangents (P : Type _) [EuclideanPlane P] where
+  left : P
+  right : P
+
+lemma tangent_circle_intersected {ω : Circle P} {p : P} (h : p LiesOut ω) : (Circle.mk_pt_pt_diam p ω.center (pt_liesout_ne_center h).symm) Intersect ω := sorry
+
+def pt_tangent_circle_pts {ω : Circle P} {p : P} (h : p LiesOut ω) : Tangents P where
+  left := (CC_Intersected_pts (tangent_circle_intersected h)).left
+  right := (CC_Intersected_pts (tangent_circle_intersected h)).right
+
+theorem tangents_lieson_circle {ω : Circle P} {p : P} (h : p LiesOut ω) : ((pt_tangent_circle_pts h).left LiesOn ω) ∧ ((pt_tangent_circle_pts h).right LiesOn ω) := by
+  rcases CC_inx_pts_lieson_circles (tangent_circle_intersected h) with ⟨_, ⟨h₂, ⟨_, h₄⟩⟩⟩
+  exact ⟨h₂, h₄⟩
+
+lemma tangents_ne_pt {ω : Circle P} {p : P} (h : p LiesOut ω) : ((pt_tangent_circle_pts h).left ≠ p) ∧ ((pt_tangent_circle_pts h).right ≠ p) := sorry
+
+theorem line_tangent_circle {ω : Circle P} {p : P} (h : p LiesOut ω) : ((DLIN p (pt_tangent_circle_pts h).left (tangents_ne_pt h).1) Tangent ω) ∧ ((DLIN p (pt_tangent_circle_pts h).right (tangents_ne_pt h).2) Tangent ω) := sorry
+
+theorem tangent_pts_eq_tangents {ω : Circle P} {p : P} (h : p LiesOut ω) : (DirLC_Tangent_pt (line_tangent_circle h).1 = (pt_tangent_circle_pts h).left) ∧ (DirLC_Tangent_pt (line_tangent_circle h).2 = (pt_tangent_circle_pts h).right) := sorry
+
+theorem length_of_tangent {ω : Circle P} {p : P} (h : p LiesOut ω) : dist p (pt_tangent_circle_pts h).left = dist p (pt_tangent_circle_pts h).right := sorry
+
+theorem tangent_and_intersected_chord_theorem {ω : Circle P} {p : P} (h : p LiesOut ω) : sorry := sorry
+
+end Circle
+
+end tangent
+
+section power
+
+namespace Circle
+
+theorem circle_power_thm {ω : Circle P} {p : P} {l : DirLine P} (h₁ : l InxWith ω) (h₂ : p LiesOn l) : sorry := sorry
+
+theorem chord_power_thm {ω : Circle P} {p : P} {l : DirLine P} (h₁ : l InxWith ω) (h₂ : p LiesOn l) (h₃ : p LiesInt ω) : (dist p (DirLC_Intersected_pts h₁).front) * (dist p (DirLC_Intersected_pts h₁).back) = - power ω p := sorry
+
+theorem secant_power_thm {ω : Circle P} {p : P} {l : DirLine P} (h₁ : l InxWith ω) (h₂ : p LiesOn l) (h₃ : p LiesOut ω) : (dist p (DirLC_Intersected_pts h₁).front) * (dist p (DirLC_Intersected_pts h₁).back) = power ω p := sorry
+
+end Circle
+
+end power
+
+section radical_axis
+
+namespace Circle
+
+end Circle
+
+end radical_axis
+
+end EuclidGeom
--- EuclideanGeometry/Foundation/Axiom/Circle/LCPosition.lean
+++ EuclideanGeometry/Foundation/Axiom/Circle/LCPosition.lean
@@ -1,6 +1,8 @@
 import EuclideanGeometry.Foundation.Axiom.Circle.Basic
 import EuclideanGeometry.Foundation.Axiom.Linear.Perpendicular
 import EuclideanGeometry.Foundation.Axiom.Linear.Perpendicular_trash
+import EuclideanGeometry.Foundation.Axiom.Triangle.Trigonometric
+import EuclideanGeometry.Foundation.Axiom.Linear.Ray_trash
 
 noncomputable section
 namespace EuclidGeom
@@ -62,13 +64,92 @@ structure DirLCInxpts (P : Type _) [EuclideanPlane P] where
   front : P
   back : P
 
+lemma dist_pt_line_ineq {l : DirLine P} {ω : Circle P} (h : l InxWith ω) : ω.radius ^ 2 - (dist_pt_line ω.center l.toLine) ^ 2 ≥ 0 := by
+  have : dist_pt_line ω.center l.toLine ≤ ω.radius := by apply DirLC_inxwith_iff_intersect.mp h
+  have : (dist_pt_line ω.center l.toLine) ^ 2 ≤ ω.radius ^ 2 := by
+    apply sq_le_sq.mpr
+    rw [abs_of_nonneg, abs_of_pos]
+    exact this
+    exact ω.rad_pos
+    unfold dist_pt_line
+    show 0 ≤ Vec.norm (VEC ω.center (perp_foot ω.center l.toLine))
+    apply Vec.norm_nonnegative
+  linarith
+
 def DirLC_Intersected_pts {l : DirLine P} {ω : Circle P} (h : l InxWith ω) : DirLCInxpts P where
   front := ((Real.sqrt (ω.radius ^ 2 - (dist_pt_line ω.center l.toLine) ^ 2)) • l.toDir.1) +ᵥ (perp_foot ω.center l.toLine)
   back := (- (Real.sqrt (ω.radius ^ 2 - (dist_pt_line ω.center l.toLine) ^ 2)) • l.toDir.1) +ᵥ (perp_foot ω.center l.toLine)
 
-theorem DirLC_inx_pts_lieson_circle {l : DirLine P} {ω : Circle P} (h : l InxWith ω) : ((DirLC_Intersected_pts h).front LiesOn ω) ∧ ((DirLC_Intersected_pts h).back LiesOn ω) := sorry
+lemma DirLC_inx_pts_lieson_dlin {l : DirLine P} {ω : Circle P} (h : l InxWith ω) : ((DirLC_Intersected_pts h).front LiesOn l) ∧ ((DirLC_Intersected_pts h).back LiesOn l) := by
+  constructor
+  · sorry
+  sorry
 
-theorem DirLC_inx_pts_same_iff_tangent {l : DirLine P} {ω : Circle P} (h : l InxWith ω) : (DirLC_Intersected_pts h).front = (DirLC_Intersected_pts h).back ↔ (l Tangent ω) := sorry
+theorem DirLC_inx_pts_lieson_circle {l : DirLine P} {ω : Circle P} (h : l InxWith ω) : ((DirLC_Intersected_pts h).front LiesOn ω) ∧ ((DirLC_Intersected_pts h).back LiesOn ω) := by
+  constructor
+  · have : (SEG ω.center (perp_foot ω.center l.toLine)).length ^ 2 + (SEG (DirLC_Intersected_pts h).front (perp_foot ω.center l.toLine)).length ^ 2 = (SEG ω.center (DirLC_Intersected_pts h).front).length ^ 2 := by apply Pythagoras_of_perp_foot _ _ (DirLC_inx_pts_lieson_dlin h).1
+    rw [← dist_pt_line] at this
+    show dist ω.center (DirLC_Intersected_pts h).front = ω.radius
+    apply (sq_eq_sq _ _).mp
+    calc
+      _ = (dist_pt_line ω.center l.toLine) ^ 2 + (SEG (DirLC_Intersected_pts h).front (perp_foot ω.center l.toLine)).length ^ 2 := by rw [← Seg.length_eq_dist, ← this]
+      _ = (dist_pt_line ω.center l.toLine) ^ 2 + (Vec.norm (VEC (DirLC_Intersected_pts h).front (perp_foot ω.center l.toLine))) ^ 2 := rfl
+      _ = (dist_pt_line ω.center l.toLine) ^ 2 + (Vec.norm ((Real.sqrt (ω.radius ^ 2 - (dist_pt_line ω.center l.toLine) ^ 2)) • l.toDir.1)) ^ 2 := by
+        unfold DirLC_Intersected_pts
+        simp
+      _ = ω.radius ^ 2 := by
+        rw [Vec.norm_smul_eq_mul_norm, Dir.norm_of_dir_tovec_eq_one, mul_one, Real.sq_sqrt]
+        ring
+        apply dist_pt_line_ineq h
+        apply Real.sqrt_nonneg
+    apply dist_nonneg
+    apply le_iff_lt_or_eq.mpr
+    left; exact ω.rad_pos
+  have : (SEG ω.center (perp_foot ω.center l.toLine)).length ^ 2 + (SEG (DirLC_Intersected_pts h).back (perp_foot ω.center l.toLine)).length ^ 2 = (SEG ω.center (DirLC_Intersected_pts h).back).length ^ 2 := by apply Pythagoras_of_perp_foot _ _ (DirLC_inx_pts_lieson_dlin h).2
+  rw [← dist_pt_line] at this
+  show dist ω.center (DirLC_Intersected_pts h).back = ω.radius
+  apply (sq_eq_sq _ _).mp
+  calc
+    _ = (dist_pt_line ω.center l.toLine) ^ 2 + (SEG (DirLC_Intersected_pts h).back (perp_foot ω.center l.toLine)).length ^ 2 := by rw [← Seg.length_eq_dist, ← this]
+    _ = (dist_pt_line ω.center l.toLine) ^ 2 + (Vec.norm (VEC (DirLC_Intersected_pts h).back (perp_foot ω.center l.toLine))) ^ 2 := rfl
+    _ = (dist_pt_line ω.center l.toLine) ^ 2 + (Vec.norm ((Real.sqrt (ω.radius ^ 2 - (dist_pt_line ω.center l.toLine) ^ 2)) • l.toDir.1)) ^ 2 := by
+        unfold DirLC_Intersected_pts
+        simp
+    _ = ω.radius ^ 2 := by
+      rw [Vec.norm_smul_eq_mul_norm, Dir.norm_of_dir_tovec_eq_one, mul_one, Real.sq_sqrt]
+      ring
+      apply dist_pt_line_ineq h
+      apply Real.sqrt_nonneg
+  apply dist_nonneg
+  apply le_iff_lt_or_eq.mpr
+  left; exact ω.rad_pos
+
+theorem DirLC_inx_pts_same_iff_tangent {l : DirLine P} {ω : Circle P} (h : l InxWith ω) : (DirLC_Intersected_pts h).front = (DirLC_Intersected_pts h).back ↔ (l Tangent ω) := by
+  unfold DirLC_Intersected_pts
+  simp
+  apply Iff.trans eq_neg_self_iff
+  apply Iff.trans mul_eq_zero
+  constructor
+  · intro hh
+    rcases hh with hh | hh
+    · show dist_pt_line ω.center l.toLine = ω.radius
+      have : ω.radius ^ 2 - dist_pt_line ω.center l.toLine ^ 2 = 0 := by
+        apply (Real.sqrt_eq_zero _).mp (Complex.ofReal_eq_zero.mp hh)
+        apply dist_pt_line_ineq h
+      apply (sq_eq_sq _ _).mp
+      linarith
+      unfold dist_pt_line
+      show 0 ≤ Vec.norm (VEC ω.center (perp_foot ω.center l.toLine))
+      apply Vec.norm_nonnegative
+      apply le_iff_lt_or_eq.mpr
+      left; exact ω.rad_pos
+    exfalso
+    have hh' : l.toDir.toVec ≠ 0 := by apply Dir.tovec_ne_zero
+    tauto
+  intro h
+  have : dist_pt_line ω.center l.toLine = ω.radius := h
+  left; rw [this, sub_self]
+  simp
 
 def DirLC_Tangent_pt {l : DirLine P} {ω : Circle P} (h : l Tangent ω) : P := (DirLC_Intersected_pts (DirLC_inxwith_iff_tangent_or_secant.mpr (Or.inl h))).front
 
@@ -98,10 +179,6 @@ protected def Line.IsSecant (l : Line P) (ω : Circle P) : Prop := dist_pt_line 
 protected def Line.IsIntersected (l : Line P) (ω : Circle P) : Prop := dist_pt_line ω.center l ≤ ω.radius
 
 end Circle
-
-scoped infix : 50 "Secant" => Circle.Line.IsSecant
-scoped infix : 50 "Tangent" => Circle.Line.IsTangent
-scoped infix : 50 "Disjoint" => Circle.Line.IsDisjoint
 
 end LC
 
