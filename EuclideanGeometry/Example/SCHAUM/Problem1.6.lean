e2e1aa1d9632cb2dcd917959c4af8b39ab14925a
xyzw12345 <wangjt2020@163.com>
2023-12-03 10:41:45 +0800

    Update Problem1.6.lean

--- EuclideanGeometry/Example/SCHAUM/Problem1.6.lean
+++ EuclideanGeometry/Example/SCHAUM/Problem1.6.lean
@@ -24,21 +24,31 @@ variable {Q : Plane} {Q_on_seg : Q LiesInt (SEG C D)}
 variable {P_Q_position : (SEG A P).length = (SEG C Q).length}
 --Prove that $PB = QD$
 theorem Problem1_6_ : (SEG P B).length = (SEG Q D).length := by
+/-
+In the parallelogram $ABCD$ we have $AB = CD$.
+Therefore, $PB = AB - AP = CD - CQ = QD$.
+-/
+  -- We have $AB = CD$ because $ABCD$ is a parallelogram
   have ab_eq_cd : (SEG A B).length = (SEG C D).length := eq_length_of_is_prg_variant hprg
   calc
   (SEG P B).length
+  -- $PB = (AP + PB) - AP$,
   _= ((SEG A P).length + (SEG P B).length) - (SEG A P).length := by ring
+  -- $AP + PB = AB$ because $P$ lies on $AB$,
   _= (SEG A B).length -  (SEG A P).length := by
     congr; symm
     apply length_eq_length_add_length
     apply Seg.lies_on_of_lies_int
     exact P_on_seg
+  -- $AB - AP = CD - CQ$ since $AB = CD$ and $AP = CQ$,
   _= (SEG C D).length - (SEG C Q).length := by
     rw [P_Q_position, ab_eq_cd]
+  -- $CD - CQ = (CQ + QD) - CQ$ because $Q$ lies on $CD$,
   _= ((SEG C Q).length + (SEG Q D).length) - (SEG C Q).length := by
     congr;
     apply length_eq_length_add_length
     apply Seg.lies_on_of_lies_int
     exact Q_on_seg
+  -- $(CQ + QD) - CQ = QD$.
   _= (SEG Q D).length := by ring
 end Problem1_6_
