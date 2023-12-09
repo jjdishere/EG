bde1b5d4f4d975c0f37576f2041bf95cd8cf8a56
kfc2333 <48116356+kfc2333@users.noreply.github.com>
2023-12-09 23:13:31 +0800

    Update Quadrilateral.lean

--- EuclideanGeometry/Foundation/Construction/Polygon/Quadrilateral.lean
+++ EuclideanGeometry/Foundation/Construction/Polygon/Quadrilateral.lean
@@ -250,8 +250,9 @@ structure is_convex_of_three_sides_two_pts_at_same_side' where
 /- Given Quadrilateral_nd qdr_nd, if qdr_nd.point₁ and qdr_nd.point₂ are at the same side of qdr_nd.nd₃₄, and it also holds for nd₄₁ and nd₁₂, then it's convex. -/
 theorem is_convex_of_three_sides_two_pts_at_same_side (p : is_convex_of_three_sides_two_pts_at_same_side' (P := P)) : p.qdr_nd IsConvex := by
   let qdr_nd := p.qdr_nd
-  by_cases h : odist_sign qdr_nd.point₁ qdr_nd.edge_nd₃₄ = 1
-  · 
+  sorry
+  -- by_cases h : odist_sign qdr_nd.point₁ qdr_nd.edge_nd₃₄ = 1
+  -- ·
 
 structure is_convex_of_diag_inx_lies_int' where
   qdr : Quadrilateral P
