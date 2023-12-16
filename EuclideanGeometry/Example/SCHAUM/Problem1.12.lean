ed120ed2950ece6432bf2ab5955fecc9f8f86363
Mosente <150507609+Mosente@users.noreply.github.com>
2023-12-16 16:43:35 +0800

    1216

--- EuclideanGeometry/Example/SCHAUM/Problem1.12.lean
+++ EuclideanGeometry/Example/SCHAUM/Problem1.12.lean
@@ -8,11 +8,35 @@ variable {Plane : Type _} [EuclideanPlane Plane]
 
 namespace SCHAUM_Problem_1_12
 /-
-Let $ABCD$ is a convex quadrilateral. Assume that the diagonal $BD \perp BC$ and $BD \perp DA$.
+Let $ABCD$ be a convex quadrilateral. Assume that the diagonal $BD \perp BC$ and $BD \perp DA$.
 Suppose that $BC = DA$.
 
 Prove that $ABCD$ is a parallelogram.
 -/
+
+
+structure Setting (Plane : Type _) [EuclideanPlane Plane] where
+  -- Let $ABCD$ be a convex quadrilateral.
+  A : Plane
+  B : Plane
+  C : Plane
+  D : Plane
+  ABCD_IsCvx : Quadrilateral.IsConvex (QDR A B C D)
+  -- $A, B, C, D$ are pairwise distinct because $ABCD$ is convex.
+  D_ne_B : D ≠ B := Quadrilateral_cvx.nd₂₄ (Quadrilateral_cvx.mk_is_convex ABCD_IsCvx)
+  C_ne_B : C ≠ B := Quadrilateral_cvx.nd₂₃ (Quadrilateral_cvx.mk_is_convex ABCD_IsCvx)
+  A_ne_D : A ≠ D := (Quadrilateral_cvx.nd₁₄ (Quadrilateral_cvx.mk_is_convex ABCD_IsCvx)).symm
+  -- Assume that the diagonal $BD \perp BC$ and $BD \perp DA$.
+  BD_perp_BC : (SEG_nd B D D_ne_B) ⟂ (SEG_nd B C C_ne_B)
+  BD_perp_DA : (SEG_nd B D D_ne_B) ⟂ (SEG_nd D A A_ne_D)
+  -- Suppose that $BC = DA$.
+  BC_eq_DA : (SEG B C).length = (SEG D A).length
+
+-- Prove that $ABCD$ is a parallelogram.
+theorem result {Plane : Type _} [EuclideanPlane Plane] (e : Setting Plane) : (QDR e.A e.B e.C e.D) IsPRG_nd := by
+  sorry
+
+
 
 -- Let $ABCD$ is a convex quadrilateral.
 variable {A B C D : Plane} {hconv : Quadrilateral.IsConvex (QDR A B C D)}
--- EuclideanGeometry/Foundation/Axiom/Linear/Ray_trash.lean
+++ EuclideanGeometry/Foundation/Axiom/Linear/Ray_trash.lean
@@ -4,19 +4,19 @@ noncomputable section
 
 namespace EuclidGeom
 
-variable {P : Type _} [EuclideanPlane P] (seg_nd : Seg_nd P)
+variable {P : Type _} [EuclideanPlane P] (seg_nd : SegND P)
 
 -- theorem same_extn_of_source_lies_int {seg_nd : SegND P} {A : P} (h : A LiesInt seg_nd) : (SEG_nd A seg_nd.target ) = seg_nd.extension
 
 -- Define the extpoint of a ray to be a point lies on the ray.toLine which has given distence to the ray.source
-def Ray.extpoint (l : Ray P) (r : ℝ) : P := r * l.toDir.toVec +ᵥ l.source
+def Ray.extpoint (l : Ray P) (r : ℝ) : P := sorry -- r * l.toDir.toVec +ᵥ l.source
 
 theorem lies_on_of_nonneg_extpoint (l : Ray P) {A : P} {r : ℝ} {hnonneg : 0 ≤ r} (h : A = Ray.extpoint l r) : A LiesOn l := sorry
 theorem lies_int_of_pos_extpoint (l : Ray P) {A : P} {r : ℝ} {hpos : 0 < r} (h : A = Ray.extpoint l r) : A LiesInt l := sorry
 theorem seg_length_eq_dist_of_extpoint (l : Ray P) {A : P} {r : ℝ} {hnonneg : 0 ≤ r} (h : A = Ray.extpoint l r): (SEG l.source A).length = r := sorry
 -- 暂时只是想实现这一功能，能够写“延长AB到C使得AB=BC”这种话，内容可能还不是很好
 
-theorem length_eq_length_add_length_of_lies_on_extension (seg_nd : Seg_nd P) {A : P} (h : A LiesOn seg_nd.extension) : (SEG seg_nd.source A).length = seg_nd.1.length + (SEG seg_nd.target A).length := sorry
+theorem length_eq_length_add_length_of_lies_on_extension (seg_nd : SegND P) {A : P} (h : A LiesOn seg_nd.extension) : (SEG seg_nd.source A).length = seg_nd.1.length + (SEG seg_nd.target A).length := sorry
 
 theorem Ray.lieson_eq_dist {A : P} {r : Ray P} (h : A LiesOn r) : VEC r.1 A = (dist r.1 A) • r.2.unitVec := by
   by_cases heq : A = r.1
