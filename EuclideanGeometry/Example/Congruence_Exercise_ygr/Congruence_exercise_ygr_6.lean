20ceda5c96998972f295888f561d5cf979e26ae2
xyzw12345 <wangjt2020@163.com>
2023-12-28 19:14:55 +0800

    upd proof of 7

--- EuclideanGeometry/Example/Congruence_Exercise_ygr/Congruence_exercise_ygr_6.lean
+++ EuclideanGeometry/Example/Congruence_Exercise_ygr/Congruence_exercise_ygr_6.lean
@@ -8,7 +8,7 @@ namespace EuclidGeom
 namespace Congruence_exercise_ygr_6
 
 /-
-In $\triangle ABC$, let $D$, $E$ be points on the segment $BC$, such that $AD = AE$ and $\angle BAD = - \angle CAE$.
+In $\triangle ABC$, let $D$, $E$ be different points on the segment $BC$, such that the points on segment $BC$ are arranged in the order $B, D, E, C$, $AD = AE$ and $\angle BAD = - \angle CAE$.
 
 Prove that $AB = AC$
 -/
@@ -26,11 +26,20 @@ structure Setting1 (Plane : Type _) [EuclideanPlane Plane] where
 -- Let $D$ be a point on the segment $BC$.
   D : Plane
   D_int_BC : D LiesInt (SEG B C)
--- Let $E$ be a point on the segment $BC$.
+-- Let $E$ be a point on the segment $BC$, different to $D$.
   E : Plane
   E_int_BC : E LiesInt (SEG B C)
+  E_ne_D : PtNe E D
+-- Claim : $E \ne B$.
+  E_ne_B : PtNe E B := ⟨ne_source_of_lies_int_seg B C E E_int_BC⟩
+-- Claim : $D \ne C$.
+  D_ne_C : PtNe D C := ⟨ne_source_of_lies_int_seg C B D (Seg.lies_int_rev_iff_lies_int.mp D_int_BC)⟩
+-- such that the points on segment $BC$ are arranged in the order $B, D, E, C$
+  D_int_BE : D LiesInt (SEG_nd B E)
+  E_int_DC : E LiesInt (SEG_nd D C)
 
-attribute [instance] Setting1.B_ne_A Setting1.C_ne_A
+
+attribute [instance] Setting1.B_ne_A Setting1.C_ne_A Setting1.E_ne_D Setting1.E_ne_B Setting1.D_ne_C
 instance D_ne_A {Plane : Type _} [EuclideanPlane Plane] {e : Setting1 Plane} : PtNe e.D e.A := by
   have h1 : ¬ colinear e.A e.B e.C := e.not_colinear_ABC
   have : e.D ≠ e.A := by
@@ -65,14 +74,9 @@ structure Setting2 (Plane : Type _) [EuclideanPlane Plane] extends Setting1 Plan
 theorem result {Plane : Type _} [EuclideanPlane Plane] (e : Setting2 Plane) : (SEG e.A e.B).length = (SEG e.A e.C).length := by
   haveI D_ne_B : PtNe e.D e.B := ⟨ne_source_of_lies_int_seg e.B e.C e.D e.D_int_BC⟩
   haveI E_ne_C : PtNe e.E e.C := ⟨ne_source_of_lies_int_seg e.C e.B e.E (Seg.lies_int_rev_iff_lies_int.mp e.E_int_BC)⟩
-  haveI C_ne_D : PtNe e.C e.D := by sorry
-  haveI E_ne_B : PtNe e.E e.B := by sorry
   have not_colinear_BAD : ¬ colinear e.B e.A e.D := by sorry
   have not_colinear_CAE : ¬ colinear e.C e.A e.E := by sorry
   have not_colinear_ADE : ¬ colinear e.A e.D e.E := by sorry
-  haveI D_ne_E : PtNe e.D e.E := by sorry
-  have E_int_DC : e.E LiesInt (SEG_nd e.D e.C) := by sorry
-  have D_int_BE : e.D LiesInt (SEG_nd e.B e.E) := by sorry
   have isoceles_ADE : (TRI e.A e.D e.E).IsIsoceles := by
     calc
     (SEG e.E e.A).length = (SEG e.A e.E).length := by apply length_of_rev_eq_length'
@@ -89,7 +93,7 @@ theorem result {Plane : Type _} [EuclideanPlane Plane] (e : Setting2 Plane) : (S
     _= (∠ e.B e.D e.E) - (∠ e.A e.D e.E) := by
       congr 1; exact ang_value_eq_ang_value_add_ang_value e.B e.A e.E e.D
     _= ↑ (π) - (∠ e.A e.D e.E) := by
-      congr 1; exact liesint_segnd_value_eq_pi' D_int_BE
+      congr 1; exact liesint_segnd_value_eq_pi' e.D_int_BE
     _= ↑ (π) + (- ∠ e.A e.D e.E) := by abel
     _= ↑ (π) + (∠ e.E e.D e.A) := by
       congr 1; symm;
@@ -98,7 +102,7 @@ theorem result {Plane : Type _} [EuclideanPlane Plane] (e : Setting2 Plane) : (S
       congr 1;
       exact is_isoceles_tri_iff_ang_eq_ang_of_nd_tri (tri_nd := (TRI_nd e.A e.D e.E not_colinear_ADE)).mp isoceles_ADE
     _= (∠ e.D e.E e.C) + (∠ e.A e.E e.D) := by
-      congr 1; symm; exact liesint_segnd_value_eq_pi' E_int_DC
+      congr 1; symm; exact liesint_segnd_value_eq_pi' e.E_int_DC
     _= (∠ e.A e.E e.D) + (∠ e.D e.E e.C) := by abel
     _= (∠ e.A e.E e.C) := by exact ang_value_eq_ang_value_add_ang_value e.A e.D e.C e.E
     _= - (∠ e.C e.E e.A) := Angle.ang_value_rev_eq_neg_value (ang := ANG e.C e.E e.A)
--- EuclideanGeometry/Example/Congruence_Exercise_ygr/Congruence_exercise_ygr_7.lean
+++ EuclideanGeometry/Example/Congruence_Exercise_ygr/Congruence_exercise_ygr_7.lean
@@ -1,4 +1,5 @@
 import EuclideanGeometry.Foundation.Index
+import EuclideanGeometry.Foundation.Axiom.Triangle.Congruence_trash
 
 noncomputable section
 
@@ -34,13 +35,79 @@ structure Setting (Plane : Type _) [EuclideanPlane Plane] where
 -- Claim : $O \ne D$.
   O_ne_D : PtNe O D := ⟨ne_source_of_lies_int_seg D A O (Seg.lies_int_rev_iff_lies_int.mpr O_int_AD)⟩
 -- If $\angle ACO = - \angle BDO$
-  angle_ACO_eq_neg_angle_BDO : ∠ A C O = - (∠ B D O)
+  angle_OCA_eq_neg_angle_ODB : ∠ O C A = - (∠ O D B)
 -- and $OA = OB$.
   OA_eq_OB : (SEG O A).length = (SEG O B).length
-
-theorem result {Plane : Type _} [EuclideanPlane Plane] (e : Setting Plane) : (SEG e.A e.D).length = (SEG e.B e.C).length := by sorry
-
-
+attribute [instance] Setting.C_ne_A Setting.B_ne_D Setting.O_ne_C Setting.O_ne_D
+theorem result {Plane : Type _} [EuclideanPlane Plane] (e : Setting Plane) : (SEG e.A e.D).length = (SEG e.B e.C).length := by
+  have not_colinear_COA : ¬ colinear e.C e.O e.A := by sorry
+  have not_colinear_DOB : ¬ colinear e.D e.O e.B := by sorry
+  haveI O_ne_A : PtNe e.O e.A := ⟨ne_source_of_lies_int_seg e.A e.D e.O e.O_int_AD⟩
+  haveI O_ne_B : PtNe e.O e.B := ⟨ne_source_of_lies_int_seg e.B e.C e.O e.O_int_BC⟩
+  haveI C_ne_B : PtNe e.C e.B := ⟨(ne_of_not_colinear e.not_colinear_ABC).1⟩
+  haveI A_ne_D : PtNe e.A e.D := ⟨((ne_of_not_colinear e.not_colinear_ABD).2.1)⟩
+  have angle_AOC_eq_neg_angle_BOD : (∠ e.A e.O e.C) = - (∠ e.B e.O e.D) := by
+    calc
+    (∠ e.A e.O e.C)
+    _= - (∠ e.C e.O e.A) := by
+      exact Angle.ang_value_rev_eq_neg_value (ang := ANG e.C e.O e.A)
+    _= - (∠ e.B e.O e.D) := by
+      simp only [neg_inj]
+      apply ang_eq_ang_of_toDir_rev_toDir
+      · calc
+        (RAY e.O e.C).toDir
+        _= - (RAY e.C e.O).toDir := by apply Ray.toDir_eq_neg_toDir_of_mk_pt_pt
+        _= - (RAY e.C e.B).toDir := by
+          congr 1;
+          apply Ray.pt_pt_toDir_eq_ray_toDir (ray := (RAY e.C e.B)) (A := e.O)
+          apply SegND.lies_int_toRay_of_lies_int (X := e.O) (seg_nd := (SEG_nd e.C e.B))
+          exact lies_int_seg_nd_of_lies_int_seg e.C e.B e.O (Seg.lies_int_rev_iff_lies_int.mpr e.O_int_BC)
+        _= (RAY e.B e.C).toDir := by symm; apply Ray.toDir_eq_neg_toDir_of_mk_pt_pt
+        _= (RAY e.B e.O).toDir := by
+          symm;
+          apply Ray.pt_pt_toDir_eq_ray_toDir (ray := (RAY e.B e.C)) (A := e.O)
+          apply SegND.lies_int_toRay_of_lies_int (X := e.O) (seg_nd := (SEG_nd e.B e.C))
+          exact lies_int_seg_nd_of_lies_int_seg e.B e.C e.O e.O_int_BC
+        _= - (RAY e.O e.B).toDir := by apply Ray.toDir_eq_neg_toDir_of_mk_pt_pt
+      · calc
+        (RAY e.O e.A).toDir
+        _= - (RAY e.A e.O).toDir := by apply Ray.toDir_eq_neg_toDir_of_mk_pt_pt
+        _= - (RAY e.A e.D).toDir := by
+          congr 1;
+          apply Ray.pt_pt_toDir_eq_ray_toDir (ray := (RAY e.A e.D)) (A := e.O)
+          apply SegND.lies_int_toRay_of_lies_int (X := e.O) (seg_nd := (SEG_nd e.A e.D))
+          exact lies_int_seg_nd_of_lies_int_seg e.A e.D e.O e.O_int_AD
+        _= (RAY e.D e.A).toDir := by symm; apply Ray.toDir_eq_neg_toDir_of_mk_pt_pt
+        _= (RAY e.D e.O).toDir := by
+          symm;
+          apply Ray.pt_pt_toDir_eq_ray_toDir (ray := (RAY e.D e.A)) (A := e.O)
+          apply SegND.lies_int_toRay_of_lies_int (X := e.O) (seg_nd := (SEG_nd e.D e.A))
+          exact lies_int_seg_nd_of_lies_int_seg e.D e.A e.O (Seg.lies_int_rev_iff_lies_int.mpr e.O_int_AD)
+        _= - (RAY e.O e.D).toDir := by apply Ray.toDir_eq_neg_toDir_of_mk_pt_pt
+  have triangle_COA_acongr_triangle_DOB : (TRI_nd e.C e.O e.A not_colinear_COA) ≅ₐ (TRI_nd e.D e.O e.B not_colinear_DOB) := by
+    apply acongr_of_AAS'
+    · exact e.angle_OCA_eq_neg_angle_ODB
+    · exact angle_AOC_eq_neg_angle_BOD
+    · exact e.OA_eq_OB
+  calc
+  (SEG e.A e.D).length
+  _= (SEG e.D e.A).length := by apply length_of_rev_eq_length'
+  _= (SEG e.D e.O).length + (SEG e.O e.A).length := by
+    apply length_eq_length_add_length
+    apply Seg.lies_on_rev_iff_lies_on.mp
+    apply Seg.lies_on_of_lies_int
+    exact e.O_int_AD
+  _= (SEG e.C e.O).length + (SEG e.O e.B).length := by
+    congr 1
+    symm; exact triangle_COA_acongr_triangle_DOB.edge₃
+    exact triangle_COA_acongr_triangle_DOB.edge₁
+  _= (SEG e.C e.B).length := by
+    symm;
+    apply length_eq_length_add_length
+    apply Seg.lies_on_rev_iff_lies_on.mp
+    apply Seg.lies_on_of_lies_int
+    exact e.O_int_BC
+  _= (SEG e.B e.C).length := by apply length_of_rev_eq_length'
 end Congruence_exercise_ygr_7
 
 end EuclidGeom
